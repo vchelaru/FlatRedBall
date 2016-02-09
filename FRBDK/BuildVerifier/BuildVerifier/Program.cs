53694d5803fe0fe5d10c30d8c4745eb5e78fc16e
Victor Chelaru <VicChelaru@gmail.com>
2016-02-08 17:24:02 -0700

    Disabled most of the BuildVerifier which I don't think buys is much.

--- FRBDK/BuildVerifier/BuildVerifier/Program.cs
+++ FRBDK/BuildVerifier/BuildVerifier/Program.cs
@@ -9,12 +9,18 @@ namespace BuildVerifier
     {
         static void Main()
         {
-            const string root = "http://www.flatredball.com/content/FrbXnaTemplates/";
+            const string root = "http://files.flatredball.com/content/FrbXnaTemplates/";
+
+            // February 8, 2016
+            // I'm disabling this for now, builds seem to be working well and everything migrated. Maybe we'll bring it back
+            // if builds start failing or if there are other problems, but I don't see the need to maintain this now.
+
+
             var files = new List<string>
                             {
-                                root + "BackupFolders.txt",
+                                //root + "BackupFolders.txt",
                                 root + "DailyBuild/FRBDK.zip",
-                                root + "DailyBuild/FlatRedBallInstaller.exe",
+                                "http://files.flatredball.com/FlatRedBallInstaller.exe",
 
                                 //root + "DailyBuild/SingleDlls/Fsb/FlatRedBall.dll",
                                 //root + "DailyBuild/SingleDlls/Fsb/SilverArcade.SilverSprite.Core.dll",
@@ -23,33 +29,33 @@ namespace BuildVerifier
                                 //root + "DailyBuild/SingleDlls/Mdx/FlatRedBallMdx.dll",
                                 //root + "DailyBuild/SingleDlls/Mdx/FlatRedBallMdx.xml",
 
-                                root + "DailyBuild/SingleDlls/MonoDroid/FlatRedBall.dll",
-                                root + "DailyBuild/SingleDlls/MonoDroid/Lidgren.Network.Android.dll",
-                                root + "DailyBuild/SingleDlls/MonoDroid/MonoGame.Framework.dll",
+                                //root + "DailyBuild/SingleDlls/MonoDroid/FlatRedBall.dll",
+                                //root + "DailyBuild/SingleDlls/MonoDroid/Lidgren.Network.Android.dll",
+                                //root + "DailyBuild/SingleDlls/MonoDroid/MonoGame.Framework.dll",
 
-                                root + "DailyBuild/SingleDlls/WindowsPhone/FlatRedBall.dll",
-                                root + "DailyBuild/SingleDlls/WindowsPhone/FlatRedBall.xml",
+                                //root + "DailyBuild/SingleDlls/WindowsPhone/FlatRedBall.dll",
+                                //root + "DailyBuild/SingleDlls/WindowsPhone/FlatRedBall.xml",
 
                                 // No more FRB XNA 3.1
                                 //root + "DailyBuild/SingleDlls/Xna/FlatRedBall.dll",
                                 //root + "DailyBuild/SingleDlls/Xna/FlatRedBall.xml",
                                 //root + "DailyBuild/SingleDlls/Xna/FlatRedBall.Content.dll",
 
-                                root + "DailyBuild/SingleDlls/Xna4_360/FlatRedBall.dll",
-                                root + "DailyBuild/SingleDlls/Xna4_360/FlatRedBall.xml",
+                                //root + "DailyBuild/SingleDlls/Xna4_360/FlatRedBall.dll",
+                                //root + "DailyBuild/SingleDlls/Xna4_360/FlatRedBall.xml",
 
-                                root + "DailyBuild/SingleDlls/Xna4Pc/FlatRedBall.dll",
-                                root + "DailyBuild/SingleDlls/Xna4Pc/FlatRedBall.xml",
-                                root + "DailyBuild/SingleDlls/Xna4Pc/FlatRedBall.Content.dll",
+                                //root + "DailyBuild/SingleDlls/Xna4Pc/FlatRedBall.dll",
+                                //root + "DailyBuild/SingleDlls/Xna4Pc/FlatRedBall.xml",
+                                //root + "DailyBuild/SingleDlls/Xna4Pc/FlatRedBall.Content.dll",
 
                                 //root + "DailyBuild/ZippedTemplates/FlatRedBallMDXTemplate.zip",
-                                root + "DailyBuild/ZippedTemplates/FlatRedBallMonoDroidTemplate.zip",
-                                root + "DailyBuild/ZippedTemplates/FlatRedBallPhoneTemplate.zip",
-                                root + "DailyBuild/ZippedTemplates/FlatRedBallXNATemplate.zip",
-                                root + "DailyBuild/ZippedTemplates/FlatRedBallXna4Template.zip",
-                                root + "DailyBuild/ZippedTemplates/FlatRedBallXna4_360Template.zip",
+                                //root + "DailyBuild/ZippedTemplates/FlatRedBallMonoDroidTemplate.zip",
+                                //root + "DailyBuild/ZippedTemplates/FlatRedBallPhoneTemplate.zip",
+                                //root + "DailyBuild/ZippedTemplates/FlatRedBallXNATemplate.zip",
+                                //root + "DailyBuild/ZippedTemplates/FlatRedBallXna4Template.zip",
+                                //root + "DailyBuild/ZippedTemplates/FlatRedBallXna4_360Template.zip",
                                 //root + "DailyBuild/ZippedTemplates/FlatSilverBallTemplate.zip",
-                                root + "DailyBuild/ZippedTemplates/GluePluginTemplate.zip"
+                                //root + "DailyBuild/ZippedTemplates/GluePluginTemplate.zip"
                             };
 
             //Add verify files
