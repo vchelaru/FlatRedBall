ca01dea60d3d8569591150e53525002e9f047acd
Victor Chelaru <VicChelaru@gmail.com>
2016-04-25 20:24:58 -0600

    Removed duplicate unneeded call.

--- Engines/FlatRedBallAddOns/FlatRedBall.TileCollisions/TileShapeCollection.cs
+++ Engines/FlatRedBallAddOns/FlatRedBall.TileCollisions/TileShapeCollection.cs
@@ -430,17 +430,6 @@ namespace FlatRedBall.TileCollisions
             }
         }
 
-
-        public static void AddCollisionFrom(this TileShapeCollection tileShapeCollection,
-            LayeredTileMap layeredTileMap)
-        {
-            foreach (var layer in layeredTileMap.MapLayers)
-            {
-                AddCollisionFrom(layer, layeredTileMap.Properties);
-            }
-        }
-
-
         public static void AddCollisionFrom(this TileShapeCollection tileShapeCollection,
             LayeredTileMap layeredTileMap)
         {
