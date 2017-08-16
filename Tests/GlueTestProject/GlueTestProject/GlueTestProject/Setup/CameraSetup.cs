6c4d07c85817c14ae52c82d334c72f25348f5701
Victor Chelaru <VicChelaru@gmail.com>
2017-08-16 08:58:45 -0600

    Converted test project to the new camera setttings window.

--- Tests/GlueTestProject/GlueTestProject/GlueTestProject/Setup/CameraSetup.cs
+++ Tests/GlueTestProject/GlueTestProject/GlueTestProject/Setup/CameraSetup.cs
@@ -1,50 +1,54 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using FlatRedBall;
-using Microsoft.Xna.Framework;
-
-#if !FRB_MDX
-using System.Linq;
-#endif
-
-namespace GlueTestProject
-{
-	internal static class CameraSetup
+	// This is a generated file created by Glue. To change this file, edit the camera settings in Glue.
+	// To access the camera settings, push the camera icon.
+	using Camera = FlatRedBall.Camera;
+	namespace GlueTestProject
 	{
-			// This is a generated file created by Glue. To change this file, edit the camera settings in Glue.
-			// To access the camera settings, push the camera icon.
-			internal static void SetupCamera (Camera cameraToSetUp, GraphicsDeviceManager graphicsDeviceManager)
+		internal static class CameraSetup
+		{
+			const float Scale = 1f;
+			internal static void ResetCamera (Camera cameraToReset)
 			{
-				SetupCamera(cameraToSetUp, graphicsDeviceManager, 800, 600);
+				FlatRedBall.Camera.Main.Orthogonal = true;
+				FlatRedBall.Camera.Main.OrthogonalHeight = 600;
+				FlatRedBall.Camera.Main.OrthogonalWidth = 800;
+				FlatRedBall.Camera.Main.FixAspectRatioYConstant();
 			}
-			internal static void SetupCamera (Camera cameraToSetUp, GraphicsDeviceManager graphicsDeviceManager, int width, int height)
+			internal static void SetupCamera (Camera cameraToSetUp, Microsoft.Xna.Framework.GraphicsDeviceManager graphicsDeviceManager, int width = 800, int height = 600)
 			{
-				#if WINDOWS
+				#if WINDOWS || DESKTOP_GL
+				FlatRedBall.FlatRedBallServices.Game.Window.AllowUserResizing = false;
 				FlatRedBall.FlatRedBallServices.GraphicsOptions.SetResolution(width, height);
 				#elif IOS || ANDROID
 				FlatRedBall.FlatRedBallServices.GraphicsOptions.SetFullScreen(FlatRedBall.FlatRedBallServices.GraphicsOptions.ResolutionWidth, FlatRedBall.FlatRedBallServices.GraphicsOptions.ResolutionHeight);
+				#elif UWP
 				#endif
-				#if WINDOWS_PHONE || WINDOWS_8 || IOS || ANDROID
-				if (height > width)
+				ResetCamera(cameraToSetUp);
+			}
+			private static void HandleResolutionChange (object sender, System.EventArgs args)
+			{
+				SetAspectRatioTo(16 / 9m);
+			}
+			private static void SetAspectRatioTo (decimal aspectRatio)
+			{
+				var resolutionAspectRatio = FlatRedBall.FlatRedBallServices.GraphicsOptions.ResolutionWidth / (decimal)FlatRedBall.FlatRedBallServices.GraphicsOptions.ResolutionHeight;
+				int destinationRectangleWidth;
+				int destinationRectangleHeight;
+				int x = 0;
+				int y = 0;
+				if (aspectRatio > resolutionAspectRatio)
 				{
-					graphicsDeviceManager.SupportedOrientations = DisplayOrientation.Portrait;
+					destinationRectangleWidth = FlatRedBall.FlatRedBallServices.GraphicsOptions.ResolutionWidth;
+					destinationRectangleHeight = FlatRedBall.Math.MathFunctions.RoundToInt(destinationRectangleWidth / (float)aspectRatio);
+					y = (FlatRedBall.FlatRedBallServices.GraphicsOptions.ResolutionHeight - destinationRectangleHeight) / 2;
 				}
 				else
 				{
-					graphicsDeviceManager.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
+					destinationRectangleHeight = FlatRedBall.FlatRedBallServices.GraphicsOptions.ResolutionHeight;
+					destinationRectangleWidth = FlatRedBall.Math.MathFunctions.RoundToInt(destinationRectangleHeight * (float)aspectRatio);
+					x = (FlatRedBall.FlatRedBallServices.GraphicsOptions.ResolutionWidth - destinationRectangleWidth) / 2;
 				}
-				#endif
-				cameraToSetUp.UsePixelCoordinates();
+				FlatRedBall.Camera.Main.DestinationRectangle = new Microsoft.Xna.Framework.Rectangle(x, y, destinationRectangleWidth, destinationRectangleHeight);
+				FlatRedBall.Camera.Main.FixAspectRatioYConstant();
 			}
-			internal static void ResetCamera (Camera cameraToReset)
-			{
-				cameraToReset.X = 0;
-				cameraToReset.Y = 0;
-				cameraToReset.XVelocity = 0;
-				cameraToReset.YVelocity = 0;
-				// Glue does not generate a detach call because the camera may be attached by this point
-			}
-
+		}
 	}
-}
