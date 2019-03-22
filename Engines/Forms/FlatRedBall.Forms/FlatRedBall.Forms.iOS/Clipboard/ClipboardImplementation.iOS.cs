ffe36b700ae875491bb69530dfca715c11c4dbec
Victor Chelaru <VicChelaru@gmail.com>
2019-03-21 23:24:10 -0600

    [Engine] Fixed iOS compilation for .Forms caused by addition of clipboard.

---
+++ Engines/Forms/FlatRedBall.Forms/FlatRedBall.Forms.iOS/Clipboard/ClipboardImplementation.iOS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace FlatRedBall.Forms.Clipboard
+{
+    public class ClipboardImplementation
+    {
+        internal static void PushStringToClipboard(string whatToCopy)
+        {
+            // not implemented
+        }
+
+        internal static string GetText()
+        {
+            return null;
+        }
+    }
+}
