324f4498e92e130e6ad964e6f90d60220d41b943
Victor Chelaru <VicChelaru@gmail.com>
2020-01-03 22:05:51 -0700

    Update AnimationSetViewModel.cs
    Deleting values from animation text box now sets null instead of ""

--- FRBDK/Glue/TopDownPlugin/ViewModels/AnimationSetViewModel.cs
+++ FRBDK/Glue/TopDownPlugin/ViewModels/AnimationSetViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using TopDownPlugin.Data;
@@ -19,50 +20,61 @@ namespace TopDownPlugin.ViewModels
         public string UpLeftAnimation
         {
             get => Get<string>();
-            set => Set(value);
+            set => SetNullIfEmpty(value);
         }
 
         public string UpAnimation
         {
             get => Get<string>();
-            set => Set(value);
+            set => SetNullIfEmpty(value);
         }
 
         public string UpRightAnimation
         {
             get => Get<string>();
-            set => Set(value);
+            set => SetNullIfEmpty(value);
         }
 
 
         public string LeftAnimation
         {
             get => Get<string>();
-            set => Set(value);
+            set => SetNullIfEmpty(value);
         }
 
         public string RightAnimation
         {
             get => Get<string>();
-            set => Set(value);
+            set => SetNullIfEmpty(value);
         }
 
         public string DownLeftAnimation
         {
             get => Get<string>();
-            set => Set(value);
+            set => SetNullIfEmpty(value);
         }
 
         public string DownAnimation
         {
             get => Get<string>();
-            set => Set(value);
+            set => SetNullIfEmpty(value);
         }
 
         public string DownRightAnimation
         {
             get => Get<string>();
-            set => Set(value);
+            set => SetNullIfEmpty(value);
+        }
+
+        // If the user hasn't entered any animations, we don't want to 
+        // generate an AnimationSet. This is easiest to do if we just check
+        // for null in the AnimationSet generation code. If a user deletes values
+        // from a text box, it will get saved as "". We'll convert here to make the
+        // rest of the code cleaner.
+        private bool SetNullIfEmpty(string value, [CallerMemberName] string caller = null)
+        {
+            value = string.IsNullOrWhiteSpace(value) ? null : value;
+            return base.Set(value, caller);
         }
 
         public AnimationSetModel BackingData
