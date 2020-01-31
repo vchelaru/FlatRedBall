1a6eb23530c73034740865356529c07b2ceb9943
Victor Chelaru <VicChelaru@gmail.com>
2020-01-31 11:40:50 -0700

    Oops, added generated files that are needed for naudio plugin.

---
+++ FRBDK/Glue/NAudioPlugin/Embedded/NAudio_Sfx.Generated.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace FlatRedBall.NAudio
+{
+    public class NAudio_Sfx: IDisposable
+    {
+        private AudioFileReader reader;
+        private WaveOut waveOut;
+
+        float volume = 1;
+        public float Volume
+        {
+            get => volume;
+            set
+            {
+                volume = value;
+                if (waveOut != null)
+                {
+                    waveOut.Volume = volume;
+                }
+            }
+        }
+
+        public NAudio_Sfx(string fileName)
+        {
+            reader = new AudioFileReader(fileName);
+            reader.Position = 0;
+            waveOut = new WaveOut();
+            waveOut.Volume = volume;
+            waveOut.Init(reader);
+            waveOut.PlaybackStopped += OnPlaybackStopped;
+        }
+
+        private void OnPlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            reader.Position = 0;
+        }
+
+        public void Play()
+        {
+            waveOut.Play();
+        }
+
+        private void TryDisposeContainedObjects()
+        {
+
+            bool needsToDispose = waveOut != null;
+
+            if (needsToDispose)
+            {
+                waveOut.Dispose();
+                reader.Dispose();
+                waveOut = null;
+                reader = null;
+            }
+        }
+
+        public void Stop()
+        {
+            if (waveOut != null)
+            {
+                waveOut.Volume = volume;
+            }
+            TryDisposeContainedObjects();
+        }
+
+        public void Dispose()
+        {
+            TryDisposeContainedObjects();
+        }
+    }
+}
---
+++ FRBDK/Glue/NAudioPlugin/Embedded/NAudio_Song.Generated.cs
@@ -0,0 +1,100 @@
+﻿using NAudio.Vorbis;
+using NAudio.Wave;
+using System;
+using System.IO;
+
+namespace FlatRedBall.NAudio
+{
+    public class NAudio_Song : IDisposable
+    {
+        VorbisWaveReader reader;
+        WaveOut waveOut;
+
+        public bool IsPlaying
+        {
+            get
+            {
+                return
+                    waveOut != null &&
+                    waveOut.PlaybackState == PlaybackState.Playing;
+            }
+        }
+
+        float volume = 1;
+        public float Volume
+        {
+            get => volume;
+            set
+            {
+                volume = value;
+                if (waveOut != null)
+                {
+                    waveOut.Volume = volume;
+                }
+            }
+        }
+        public bool IsRepeating { get; set; } = true;
+
+        public NAudio_Song (string fileName) 
+        {
+            this.reader = new VorbisWaveReader(fileName);
+            this.reader.Position = 0;
+            this.waveOut = new WaveOut();
+
+            this.waveOut.Init(reader);
+            this.waveOut.Volume = volume;
+            this.waveOut.PlaybackStopped += HandlePlaybackStopped;
+        }
+
+        public void Play()
+        {
+            this.waveOut.Play();
+        }
+
+        private void HandlePlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            if (IsRepeating)
+            {
+                StartOver();
+            }
+            else
+            {
+                TryDisposeContainedObjects();
+            }
+        }
+
+        public void StartOver()
+        {
+            this.reader.Position = 0;
+            this.waveOut.Play();
+        }
+
+        private void TryDisposeContainedObjects()
+        {
+
+            bool needsToDispose = waveOut != null;
+
+            if (needsToDispose)
+            {
+                waveOut.Dispose();
+                reader.Dispose();
+                waveOut = null;
+                reader = null;
+            }
+        }
+
+        public void Stop()
+        {
+            if (waveOut != null)
+            {
+                waveOut.Volume = volume;
+            }
+            TryDisposeContainedObjects();
+        }
+
+        public void Dispose()
+        {
+            TryDisposeContainedObjects();
+        }
+    }
+}
