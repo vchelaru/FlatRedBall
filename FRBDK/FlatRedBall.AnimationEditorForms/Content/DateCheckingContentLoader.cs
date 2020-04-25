0c54cd9c7a8e12094266d7d4e073efc947f90b12
Victor Chelaru <VicChelaru@gmail.com>
2020-04-24 21:24:23 -0600

    Oops forgot last files.

---
+++ FRBDK/FlatRedBall.AnimationEditorForms/Content/DateCheckingContentLoader.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using RenderingLibrary;
+using RenderingLibrary.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolsUtilities;
+
+namespace FlatRedBall.AnimationEditorForms.Content
+{
+    public class DateCheckingContentLoader : IContentLoader
+    {
+        Dictionary<string, IDisposable> disposables = new Dictionary<string, IDisposable>();
+        SystemManagers systemManagers;
+
+        public DateCheckingContentLoader(SystemManagers systemManagers)
+        {
+            this.systemManagers = systemManagers;
+        }
+
+        public void AddDisposable(string contentName, IDisposable disposable)
+        {
+            if (disposables.ContainsKey(contentName))
+            {
+                throw new Exception("This item has already been added");
+            }
+            else
+            {
+                disposables.Add(contentName, disposable);
+            }
+        }
+
+        public T LoadContent<T>(string contentName)
+        {
+            string fileNameStandardized = FileManager.Standardize(contentName, false, false);
+
+            if (FileManager.IsRelative(fileNameStandardized))
+            {
+                fileNameStandardized = FileManager.RelativeDirectory + fileNameStandardized;
+
+                fileNameStandardized = FileManager.RemoveDotDotSlash(fileNameStandardized);
+            }
+
+            if(disposables.ContainsKey(fileNameStandardized))
+            {
+                return (T)(object)disposables[fileNameStandardized];
+            }
+
+            if (typeof(T) == typeof(Texture2D))
+            {
+                var texture = LoaderManager.Self.LoadTextureFromFile(
+                    contentName, systemManagers);
+
+
+                AddDisposable(contentName, texture);
+                return (T)(object)texture;
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public T TryGetCachedDisposable<T>(string contentName)
+        {
+            if (disposables.ContainsKey(contentName))
+            {
+                return (T)disposables[contentName];
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        public T TryLoadContent<T>(string contentName)
+        {
+            try
+            {
+                return LoadContent<T>(contentName);
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+    }
+}
