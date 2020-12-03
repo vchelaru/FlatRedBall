c138bee16e259846654a52cae1902b6be9e440d3
Victor Chelaru <VicChelaru@gmail.com>
2020-12-03 08:45:14 -0700

    Added FactoryManager.Get which returns a factory by name.

--- FRBDK/Glue/Glue/Resources/IEntityFactory.cs
+++ FRBDK/Glue/Glue/Resources/IEntityFactory.cs
@@ -13,4 +13,79 @@ namespace REPLACED_NAMESPACE
         void Initialize(string contentManager);
         void ClearListsToAddTo();
     }
+
+
+    public static class FactoryManager
+    {
+        static Dictionary<string, IEntityFactory> factoryDictionary = new Dictionary<string, IEntityFactory>();
+        public static IEntityFactory Get(string entityName)
+        {
+            if (factoryDictionary.ContainsKey(entityName))
+            {
+                return factoryDictionary[entityName];
+            }
+            else
+            {
+                var factory = FindFactory(entityName);
+                factoryDictionary.Add(entityName, factory);
+                return factory;
+            }
+        }
+
+        static Type[] typesInThisAssembly;
+        public static IEntityFactory FindFactory(string entityType)
+        {
+            if (typesInThisAssembly == null)
+            {
+#if WINDOWS_8 || UWP
+                var assembly = typeof(TileEntityInstantiator).GetTypeInfo().Assembly;
+                typesInThisAssembly = assembly.DefinedTypes.Select(item=>item.AsType()).ToArray();
+
+#else
+                var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                typesInThisAssembly = assembly.GetTypes();
+#endif
+            }
+
+
+#if WINDOWS_8 || UWP
+            var filteredTypes =
+                typesInThisAssembly.Where(t => t.GetInterfaces().Contains(typeof(IEntityFactory))
+                            && t.GetConstructors().Any(c=>c.GetParameters().Count() == 0));
+#else
+            var filteredTypes =
+                typesInThisAssembly.Where(t => t.GetInterfaces().Contains(typeof(IEntityFactory))
+                            && t.GetConstructor(Type.EmptyTypes) != null);
+#endif
+
+            var factories = filteredTypes
+                .Select(
+                    t =>
+                    {
+#if WINDOWS_8 || UWP
+                        var propertyInfo = t.GetProperty("Self");
+#else
+                        var propertyInfo = t.GetProperty("Self");
+#endif
+                        var value = propertyInfo.GetValue(null, null);
+                        return value as IEntityFactory;
+                    }).ToList();
+
+
+            var factory = factories.FirstOrDefault(item =>
+            {
+                var type = item.GetType();
+                var methodInfo = type.GetMethod("CreateNew", new[] { typeof(FlatRedBall.Graphics.Layer), typeof(float), typeof(float) });
+                var returntypeString = methodInfo.ReturnType.Name;
+
+                return entityType == returntypeString ||
+                    entityType.EndsWith("\\" + returntypeString) ||
+                    entityType.EndsWith("/" + returntypeString);
+            });
+            return factory;
+
+        }
+
+
+    }
 }
