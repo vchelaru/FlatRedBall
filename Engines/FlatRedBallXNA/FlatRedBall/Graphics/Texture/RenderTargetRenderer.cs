d81e3c4040fdf2884604908321c6300d89f1519d
Victor Chelaru <VicChelaru@gmail.com>
2020-12-17 07:35:21 -0700

    Moved RendertergetRenderer and added RenderTargetSprite to the engine.

---
+++ Engines/FlatRedBallXNA/FlatRedBall/Graphics/Texture/RenderTargetRenderer.cs
@@ -0,0 +1,120 @@
+﻿using FlatRedBall;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlatRedBall.Graphics
+{
+    public class RenderTargetRenderer
+    {
+        int mWidth;
+        int mHeight;
+
+        RenderTarget2D mRenderTarget;
+
+        public Texture2D Texture
+        {
+            get
+            {
+                if(!HasRendered)
+                {
+                    throw new InvalidOperationException("You must first call PerformRender");
+                }
+                return mRenderTarget;
+            }
+        }
+
+        public Camera Camera
+        {
+            get;
+            private set;
+        }
+
+        public FlatRedBall.Graphics.Layer Layer
+        {
+            get =>Camera.Layer;
+        }
+
+        public bool HasRendered
+        {
+            get;
+            private set;
+        }
+
+        public RenderTargetRenderer(int width, int height, bool generateMipMaps = false)
+        {
+            mWidth = width;
+            mHeight = height;
+
+            var device = FlatRedBallServices.GraphicsDevice;
+            mRenderTarget = new RenderTarget2D(device, mWidth, mHeight,
+                generateMipMaps, device.DisplayMode.Format, DepthFormat.Depth24);
+
+            Camera = new Camera(null, mWidth, mHeight);
+            Camera.UsePixelCoordinates();
+            Camera.Z = 40;
+            Camera.DrawsWorld = false;
+
+        }
+
+        public RenderTargetRenderer(Camera camera, bool generateMipMaps = false)
+        {
+            // The camera may be off-center. Even though the area to the left and above the camera are not used, 
+            // we need to pad the render target renderer so that internally FRB doesn't crash:
+            mWidth = camera.DestinationRectangle.Left + camera.DestinationRectangle.Width;
+            mHeight = camera.DestinationRectangle.Top + camera.DestinationRectangle.Height;
+
+            var device = FlatRedBallServices.GraphicsDevice;
+            mRenderTarget = new RenderTarget2D(device, mWidth, mHeight);
+            // Monogame does not like these extra parameters. They cause some kind of "imbalance the stack" exception
+            //,
+              //  generateMipMaps, device.DisplayMode.Format, DepthFormat.Depth24);
+
+            this.Camera = camera;
+        }
+
+        public void ReRender()
+        {
+            if(!HasRendered)
+            {
+                throw new InvalidOperationException("You must first call PerformRender");
+            }
+
+            // This updates the internal rendering variables according to what the user has set.
+            Camera.ForceUpdateDependencies();
+
+
+            var device = FlatRedBallServices.GraphicsDevice;
+
+            // Default alpha blend
+            device.BlendState = BlendState.AlphaBlend;
+
+            device.Viewport = Camera.GetViewport();
+            device.SetRenderTarget(mRenderTarget);
+            device.Clear(Microsoft.Xna.Framework.Color.Transparent);
+            FlatRedBall.Graphics.Renderer.DrawCamera(Camera, null);
+            device.SetRenderTarget(null);
+
+        }
+
+        public void PerformRender(string contentManagerName, string textureName)
+        {
+            if (HasRendered)
+            {
+                throw new InvalidOperationException("This has already rendered once, call ReRender instead.");
+            }
+            HasRendered = true;
+
+            ReRender();
+
+            mRenderTarget.Name = textureName;
+
+            var contentManager = FlatRedBallServices.GetContentManagerByName(contentManagerName);
+            contentManager.AddDisposable(textureName, mRenderTarget);
+
+           
+        }
+    }
+}
---
+++ Engines/FlatRedBallXNA/FlatRedBall/Graphics/Texture/RenderTargetSprite.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlatRedBall.Graphics
+{
+    public class RenderTargetSprite : Sprite
+    {
+        RenderTargetRenderer mRenderer;
+
+        string mContentManagerName;
+
+        public Layer DefaultInputLayer => mRenderer.Layer;
+
+        /// <summary>
+        /// Instantiates a new RenderTargetSprite with a render target matching the spriteName belonging
+        /// to the argument ContentManagerName
+        /// </summary>
+        /// <param name="contentManagerName">The content manager name which the RenderTarget should belong to. This
+        /// is used to dispose the RenderTarget, and will usually match the current screen's content manager name.</param>
+        /// <param name="spriteName">The name of the Sprite, which is shared with the render target. This is required </param>
+        public RenderTargetSprite(string contentManagerName, string spriteName)
+        {
+            mContentManagerName = contentManagerName;
+
+            mRenderer = new RenderTargetRenderer(Camera.Main.DestinationRectangle.Width,
+                Camera.Main.DestinationRectangle.Height);
+
+            this.TextureScale = 1;
+            this.AttachTo(Camera.Main, false);
+            this.RelativeX = 0;
+            this.RelativeY = 0;
+            this.RelativeZ = -40;
+        }
+
+        public void Refresh()
+        {
+            Camera.Main.ForceUpdateDependencies();
+            mRenderer.Camera.Position = Camera.Main.Position;
+            mRenderer.Camera.Orthogonal = Camera.Main.Orthogonal;
+            mRenderer.Camera.AspectRatio = Camera.Main.AspectRatio;
+            mRenderer.Camera.FieldOfView = Camera.Main.FieldOfView;
+            mRenderer.Camera.OrthogonalWidth = Camera.Main.OrthogonalWidth;
+            mRenderer.Camera.OrthogonalHeight = Camera.Main.OrthogonalHeight;
+
+
+            if(mRenderer.HasRendered)
+            {
+                mRenderer.ReRender();
+            }
+            else
+            {
+                mRenderer.PerformRender(mContentManagerName, mName);
+                Texture = mRenderer.Texture;
+            }
+        }
+
+        public void AddInputLayer(Layer layer)
+        {
+            if(SpriteManager.Layers.Contains(layer))
+            {
+                SpriteManager.RemoveLayer(layer);
+            }
+            mRenderer.Camera.AddLayer(layer);
+        }
+    }
+}
