92b5b58cfaa71dcfe64e43c5f01ad6188e175890
Victor Chelaru <VicChelaru@gmail.com>
2021-04-30 17:36:36 -0600

    Reorganized CharacterJoiningScreen for walkthrough.

--- Samples/Platformer/MultiplayerPlatformerDemo/MultiplayerPlatformerDemo/Screens/CharacterJoiningScreen.cs
+++ Samples/Platformer/MultiplayerPlatformerDemo/MultiplayerPlatformerDemo/Screens/CharacterJoiningScreen.cs
@@ -24,49 +24,45 @@ namespace MultiplayerPlatformerDemo.Screens
         void CustomInitialize()
         {
             var gum = CharacterJoiningScreenGum;
+            gum.BindingContext = new CharacterJoiningScreenViewModel(); 
 
-            var vm = new CharacterJoiningScreenViewModel();
+            gum.IndividualJoinComponentInstance.BindingContext =  ViewModel.IndividualJoinViewModels[0];
+            gum.IndividualJoinComponentInstance1.BindingContext = ViewModel.IndividualJoinViewModels[1];
+            gum.IndividualJoinComponentInstance2.BindingContext = ViewModel.IndividualJoinViewModels[2];
+            gum.IndividualJoinComponentInstance3.BindingContext = ViewModel.IndividualJoinViewModels[3];
 
-            for(int i = 0; i < vm.IndividualJoinViewModels.Length; i++)
+            for(int i = 0; i < ViewModel.IndividualJoinViewModels.Length; i++)
             {
                 if(InputManager.Xbox360GamePads[i].IsConnected)
                 {
                     if(GameScreen.PlayerJoinStates[i] == GumRuntimes.IndividualJoinComponentRuntime.JoinCategory.Joined)
                     {
-                        vm.IndividualJoinViewModels[i].JoinState = GumRuntimes.IndividualJoinComponentRuntime.JoinCategory.Joined;
+                        ViewModel.IndividualJoinViewModels[i].JoinState = GumRuntimes.IndividualJoinComponentRuntime.JoinCategory.Joined;
                     }
                     else
                     {
-                        vm.IndividualJoinViewModels[i].JoinState = GumRuntimes.IndividualJoinComponentRuntime.JoinCategory.PluggedInNotJoined;
+                        ViewModel.IndividualJoinViewModels[i].JoinState = GumRuntimes.IndividualJoinComponentRuntime.JoinCategory.PluggedInNotJoined;
                     }
                 }
             }
-
-            gum.BindingContext = vm;
-
-            gum.IndividualJoinComponentInstance.BindingContext = vm.IndividualJoinViewModels[0];
-            gum.IndividualJoinComponentInstance1.BindingContext = vm.IndividualJoinViewModels[1];
-            gum.IndividualJoinComponentInstance2.BindingContext = vm.IndividualJoinViewModels[2];
-            gum.IndividualJoinComponentInstance3.BindingContext = vm.IndividualJoinViewModels[3];
-
 
             InputManager.ControllerConnectionEvent += HandleControllerConnectionEvent;
         }
 
         private void HandleControllerConnectionEvent(object sender, InputManager.ControllerConnectionEventArgs e)
         {
-            if(e.Connected)
+            var individualVm = ViewModel.IndividualJoinViewModels[e.PlayerIndex];
+            if (e.Connected)
             {
-                if(ViewModel.IndividualJoinViewModels[e.PlayerIndex].JoinState == GumRuntimes.IndividualJoinComponentRuntime.JoinCategory.NotPluggedIn)
+                if(individualVm.JoinState == GumRuntimes.IndividualJoinComponentRuntime.JoinCategory.NotPluggedIn)
                 {
-                    ViewModel.IndividualJoinViewModels[e.PlayerIndex].JoinState = GumRuntimes.IndividualJoinComponentRuntime.JoinCategory.PluggedInNotJoined;
+                    individualVm.JoinState = GumRuntimes.IndividualJoinComponentRuntime.JoinCategory.PluggedInNotJoined;
                 }
             }
             else // disconnected
             {
-                ViewModel.IndividualJoinViewModels[e.PlayerIndex].JoinState = GumRuntimes.IndividualJoinComponentRuntime.JoinCategory.NotPluggedIn;
+                individualVm.JoinState = GumRuntimes.IndividualJoinComponentRuntime.JoinCategory.NotPluggedIn;
             }
-
         }
 
         void CustomActivity(bool firstTimeCalled)
@@ -99,7 +95,6 @@ namespace MultiplayerPlatformerDemo.Screens
                     }
                 }
             }
-
         }
 
         private void StartLevel()
