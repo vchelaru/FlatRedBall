85dd175047b0fe5b4da4bf5952424e4be809b806
Victor Chelaru <VicChelaru@gmail.com>
2021-10-08 09:29:46 -0600

    Build output no longer adds extra newlines unnecessarily.

--- FRBDK/Glue/OfficialPlugins/Compiler/MainControl.xaml.cs
+++ FRBDK/Glue/OfficialPlugins/Compiler/MainControl.xaml.cs
@@ -74,14 +74,18 @@ namespace OfficialPlugins.Compiler
                 var paragraph = TextBox.Document.Blocks.Last() as Paragraph;
                 foreach (var line in split)
                 {
-                    
-                    var outputType = outputParser.GetOutputType(line);
-                    if(outputType != OutputType.Warning)
+                    if(!string.IsNullOrWhiteSpace(line))
                     {
-                        var color = outputType == OutputType.Error ? Brushes.Red : Brushes.Black;
-                        paragraph.Inlines.Add(new Run(line + "\r\n") { Foreground = color});
+                        var outputType = outputParser.GetOutputType(line);
+                        if(outputType != OutputType.Warning)
+                        {
+                            var color = outputType == OutputType.Error ? Brushes.Red : Brushes.Black;
+                            paragraph.Inlines.Add(new Run(line + "\r\n") { Foreground = color});
+                        }
                     }
                 }
+
+                TextBox.ScrollToEnd();
             });
         }
 
