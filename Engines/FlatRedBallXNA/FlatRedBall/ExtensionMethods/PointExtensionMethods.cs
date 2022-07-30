0917192f73faef7318c08d7950a66f71edbe4f23
Victor Chelaru <VicChelaru@gmail.com>
2022-07-30 12:58:27 -0600

    Huge increases in Polygon Point assignment and IsConvex calls for polygons with a lot of points.

---
+++ Engines/FlatRedBallXNA/FlatRedBall/ExtensionMethods/PointExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlatRedBall.Math.Geometry
+{
+    public static class PointExtensionMethods
+    {
+        public static float? Angle(this Point vector)
+        {
+            if (vector.X == 0 && vector.Y == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return (float)System.Math.Atan2(vector.Y, vector.X);
+            }
+        }
+    }
+}
