17096cc07aa4bb8ac37c59cf500f1fd91a29ea0a
Victor Chelaru <VicChelaru@gmail.com>
2022-09-09 14:14:27 -0600

    Added .NET 6 Template.

---
+++ Templates/FlatRedBallDesktopGlNet6Template/FlatRedBallDesktopGlTemplate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FlatRedBallDesktopGlTemplate
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+            {
+                try
+                {
+                    game.Run();
+                }
+                catch (Exception e)
+                {
+                    System.IO.File.WriteAllText("CrashInfo.txt", e.ToString());
+                    throw;
+                }
+
+            }
+        }
+    }
+}
