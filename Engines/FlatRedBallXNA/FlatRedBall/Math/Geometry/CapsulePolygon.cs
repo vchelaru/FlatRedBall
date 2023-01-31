ee86d4a8f34fb27f6a3b7cf722ebc34be6b52c50
Miguel Anxo Figueirido Prado <miguelfigueirido@hotmail.com>
2023-01-31 14:16:39 +0100

    Use UpdateDependencies pattern for CapsulePolygon rotation.

--- Engines/FlatRedBallXNA/FlatRedBall/Math/Geometry/CapsulePolygon.cs
+++ Engines/FlatRedBallXNA/FlatRedBall/Math/Geometry/CapsulePolygon.cs
@@ -8,7 +8,7 @@ namespace FlatRedBall.Math.Geometry
     public class CapsulePolygon : Polygon
     {
         bool mSupressPointsRecalculation;
-        Point[] mUnrotatedPoints;
+        Point[] mPoints;
 
         float mWidth;
         public float Width
@@ -27,7 +27,7 @@ namespace FlatRedBall.Math.Geometry
 
                     if (changed)
                     {
-                        RecalculatePoints(true);
+                        RecalculatePoints();
                     }
                 }
             }
@@ -50,7 +50,7 @@ namespace FlatRedBall.Math.Geometry
 
                     if (changed)
                     {
-                        RecalculatePoints(true);
+                        RecalculatePoints();
                     }
                 }
             }
@@ -77,26 +77,8 @@ namespace FlatRedBall.Math.Geometry
 
                     if (changed)
                     {
-                        RecalculatePoints(true);
+                        RecalculatePoints();
                     }
-                }
-            }
-        }
-
-        public new float RotationZ
-        {
-            get
-            {
-                return base.RotationZ;
-            }
-            set
-            {
-                bool changed = base.RotationZ != value;
-                base.RotationZ = value;
-
-                if (changed)
-                {
-                    RecalculatePoints(false);
                 }
             }
         }
@@ -119,108 +101,109 @@ namespace FlatRedBall.Math.Geometry
 
             mSupressPointsRecalculation = false;
 
-            RecalculatePoints(true);
+            RecalculatePoints();
         }
 
-        void RecalculatePoints(bool recalculateShape)
+        void RecalculatePoints()
         {
             if (mSupressPointsRecalculation)
             {
                 return;
             }
 
-            // Only recreate the shape if the width, height or number of segments change
-            if (recalculateShape)
+            // Miguel: I'm creating the capsule in a linear fashion. One straight line
+            // followed by a semicircle, another straight line and another semicircle.
+
+            int semicircleNumberOfPoints = SemicircleNumberOfPoints;
+            mPoints = new Point[NumberOfShapePoints];
+
+            float halfWidth = mWidth / 2;
+            float halfHeight = mHeight / 2;
+            float semicircleCenter;
+            float radiansPerSemicircleStep = MathHelper.Pi / mSemicircleNumberOfSegments;
+
+            Vector2 s1, s2, r1, r2, r3, r4, s1Direction, s2Direction;
+
+            bool horizontal = mWidth > mHeight;
+
+            if (horizontal)
             {
-                // Miguel: I'm creating the capsule in a linear fashion. One straight line
-                // followed by a semicircle, another straight line and another semicircle.
-
-                int semicircleNumberOfPoints = SemicircleNumberOfPoints;
-                mUnrotatedPoints = new Point[NumberOfShapePoints];
-
-                float halfWidth = mWidth / 2;
-                float halfHeight = mHeight / 2;
-                float semicircleCenter;
-                float radiansPerSemicircleStep = MathHelper.Pi / mSemicircleNumberOfSegments;
-
-                Vector2 s1, s2, r1, r2, r3, r4, s1Direction, s2Direction;
-
-                bool horizontal = mWidth > mHeight;
-
-                if (horizontal)
-                {
-                    semicircleCenter = halfWidth - halfHeight;
-                    s1 = new Vector2(semicircleCenter, 0);
-                    s2 = new Vector2(-semicircleCenter, 0);
-                    r1 = new Vector2(-semicircleCenter, halfHeight);
-                    r2 = new Vector2(semicircleCenter, halfHeight);
-                    r3 = new Vector2(semicircleCenter, -halfHeight);
-                    r4 = new Vector2(-semicircleCenter, -halfHeight);
-                    s1Direction = new Vector2(0, -halfHeight);
-                    s2Direction = new Vector2(0, halfHeight);
-                }
-                else // Vertical
-                {
-                    semicircleCenter = halfHeight - halfWidth;
-                    s1 = new Vector2(0, semicircleCenter);
-                    s2 = new Vector2(0, -semicircleCenter);
-                    r1 = new Vector2(-halfWidth, -semicircleCenter);
-                    r2 = new Vector2(-halfWidth, semicircleCenter);
-                    r3 = new Vector2(halfWidth, semicircleCenter);
-                    r4 = new Vector2(halfWidth, -semicircleCenter);
-                    s1Direction = new Vector2(-halfWidth, 0);
-                    s2Direction = new Vector2(halfWidth, 0);
-                }
-
-                // Add first straight points
-                mUnrotatedPoints[0] = new Point(ref r1);
-                mUnrotatedPoints[1] = new Point(ref r2);
-                int lastPointInserted = 1;
-
-                // Add first semicircle points
-                for (int i = 0; i < semicircleNumberOfPoints; i++)
-                {
-                    float rotationAmount = (i + 1) * radiansPerSemicircleStep * (horizontal ? 1f : -1f);
-                    var rotation = Matrix.CreateRotationZ(rotationAmount);
-                    var rotatedVector = Vector2.Transform(s1Direction, rotation);
-                    var finalPosition = s1 + rotatedVector;
-                    finalPosition.Y *= !horizontal ? 1f : -1f;
-                    mUnrotatedPoints[lastPointInserted + i + 1] = new Point(ref finalPosition);
-                }
-
-                // Add second straight points
-                lastPointInserted = 1 + semicircleNumberOfPoints;
-                mUnrotatedPoints[lastPointInserted + 1] = new Point(ref r3);
-                mUnrotatedPoints[lastPointInserted + 2] = new Point(ref r4);
-                lastPointInserted += 2;
-
-                // Add second semicircle points
-                for (int i = 0; i < semicircleNumberOfPoints; i++)
-                {
-                    float rotationAmount = (i + 1) * radiansPerSemicircleStep * (horizontal ? 1f : -1f);
-                    var rotation = Matrix.CreateRotationZ(rotationAmount);
-                    var rotatedVector = Vector2.Transform(s2Direction, rotation);
-                    var finalPosition = s2 + rotatedVector;
-                    finalPosition.Y *= !horizontal ? 1f : -1f;
-                    mUnrotatedPoints[lastPointInserted + i + 1] = new Point(ref finalPosition);
-                }
-
-                lastPointInserted += semicircleNumberOfPoints;
-
-                // Add closing point
-                mUnrotatedPoints[lastPointInserted + 1] = new Point(ref r1);
+                semicircleCenter = halfWidth - halfHeight;
+                s1 = new Vector2(semicircleCenter, 0);
+                s2 = new Vector2(-semicircleCenter, 0);
+                r1 = new Vector2(-semicircleCenter, halfHeight);
+                r2 = new Vector2(semicircleCenter, halfHeight);
+                r3 = new Vector2(semicircleCenter, -halfHeight);
+                r4 = new Vector2(-semicircleCenter, -halfHeight);
+                s1Direction = new Vector2(0, -halfHeight);
+                s2Direction = new Vector2(0, halfHeight);
+            }
+            else // Vertical
+            {
+                semicircleCenter = halfHeight - halfWidth;
+                s1 = new Vector2(0, semicircleCenter);
+                s2 = new Vector2(0, -semicircleCenter);
+                r1 = new Vector2(-halfWidth, -semicircleCenter);
+                r2 = new Vector2(-halfWidth, semicircleCenter);
+                r3 = new Vector2(halfWidth, semicircleCenter);
+                r4 = new Vector2(halfWidth, -semicircleCenter);
+                s1Direction = new Vector2(-halfWidth, 0);
+                s2Direction = new Vector2(halfWidth, 0);
             }
 
+            // Add first straight points
+            mPoints[0] = new Point(ref r1);
+            mPoints[1] = new Point(ref r2);
+            int lastPointInserted = 1;
+
+            // Add first semicircle points
+            for (int i = 0; i < semicircleNumberOfPoints; i++)
+            {
+                float rotationAmount = (i + 1) * radiansPerSemicircleStep * (horizontal ? 1f : -1f);
+                var rotation = Matrix.CreateRotationZ(rotationAmount);
+                var rotatedVector = Vector2.Transform(s1Direction, rotation);
+                var finalPosition = s1 + rotatedVector;
+                finalPosition.Y *= !horizontal ? 1f : -1f;
+                mPoints[lastPointInserted + i + 1] = new Point(ref finalPosition);
+            }
+
+            // Add second straight points
+            lastPointInserted = 1 + semicircleNumberOfPoints;
+            mPoints[lastPointInserted + 1] = new Point(ref r3);
+            mPoints[lastPointInserted + 2] = new Point(ref r4);
+            lastPointInserted += 2;
+
+            // Add second semicircle points
+            for (int i = 0; i < semicircleNumberOfPoints; i++)
+            {
+                float rotationAmount = (i + 1) * radiansPerSemicircleStep * (horizontal ? 1f : -1f);
+                var rotation = Matrix.CreateRotationZ(rotationAmount);
+                var rotatedVector = Vector2.Transform(s2Direction, rotation);
+                var finalPosition = s2 + rotatedVector;
+                finalPosition.Y *= !horizontal ? 1f : -1f;
+                mPoints[lastPointInserted + i + 1] = new Point(ref finalPosition);
+            }
+
+            lastPointInserted += semicircleNumberOfPoints;
+
+            // Add closing point
+            mPoints[lastPointInserted + 1] = new Point(ref r1);
+
+            UpdatePolygonPoints();
+        }
+
+        void UpdatePolygonPoints()
+        {
             if (mRotationZ == 0)
             {
-                base.Points = mUnrotatedPoints;
+                base.Points = mPoints;
             }
             else
             {
                 // Make a copy of the unrotated points and apply rotation to the copy.
                 // This allows us to skip shape recreation when only RotationZ changes.
                 var rotatedPoints = new Point[NumberOfShapePoints];
-                mUnrotatedPoints.CopyTo(rotatedPoints, 0);
+                mPoints.CopyTo(rotatedPoints, 0);
 
                 for (int i = 0; i < rotatedPoints.Length; i++)
                 {
@@ -229,6 +212,12 @@ namespace FlatRedBall.Math.Geometry
 
                 base.Points = rotatedPoints;
             }
+        }
+
+        public override void UpdateDependencies(double currentTime)
+        {
+            base.UpdateDependencies(currentTime);
+            UpdatePolygonPoints();
         }
     }
 }
