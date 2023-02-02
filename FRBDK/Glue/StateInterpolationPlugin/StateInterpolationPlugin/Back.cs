bf6cc123565332c0a6f87767e8dfe055104f469a
Victor Chelaru <VicChelaru@gmail.com>
2023-02-02 07:15:46 -0700

    Fixed variable naming.

--- FRBDK/Glue/StateInterpolationPlugin/StateInterpolationPlugin/Back.cs
+++ FRBDK/Glue/StateInterpolationPlugin/StateInterpolationPlugin/Back.cs
@@ -6,24 +6,24 @@ namespace FlatRedBall.Glue.StateInterpolation
 {
     public static class Back
     {
-        public static float EaseIn(float timeElapsed, float startingValue, float endingValue, float durationInSeconds)
+        public static float EaseIn(float timeElapsed, float startingValue, float amountToAdd, float durationInSeconds)
         {
-            return endingValue * (timeElapsed /= durationInSeconds) * timeElapsed * ((1.70158f + 1) * timeElapsed - 1.70158f) + startingValue;
+            return amountToAdd * (timeElapsed /= durationInSeconds) * timeElapsed * ((1.70158f + 1) * timeElapsed - 1.70158f) + startingValue;
         }
 
-        public static float EaseOut(float timeElapsed, float startingValue, float endingValue, float durationInSeconds)
+        public static float EaseOut(float timeElapsed, float startingValue, float amountToAdd, float durationInSeconds)
         {
-            return endingValue * ((timeElapsed = timeElapsed / durationInSeconds - 1) * timeElapsed * ((1.70158f + 1) * timeElapsed + 1.70158f) + 1) + startingValue;
+            return amountToAdd * ((timeElapsed = timeElapsed / durationInSeconds - 1) * timeElapsed * ((1.70158f + 1) * timeElapsed + 1.70158f) + 1) + startingValue;
         }
 
-        public static float EaseInOut(float timeElapsed, float startingValue, float endingValue, float durationInSeconds)
+        public static float EaseInOut(float timeElapsed, float startingValue, float amountToAdd, float durationInSeconds)
         {
             float s = 1.70158f;
             if ((timeElapsed /= durationInSeconds / 2) < 1)
             {
-                return endingValue / 2 * (timeElapsed * timeElapsed * (((s *= (1.525f)) + 1) * timeElapsed - s)) + startingValue;
+                return amountToAdd / 2 * (timeElapsed * timeElapsed * (((s *= (1.525f)) + 1) * timeElapsed - s)) + startingValue;
             }
-            return endingValue / 2 * ((timeElapsed -= 2) * timeElapsed * (((s *= (1.525f)) + 1) * timeElapsed + s) + 2) + startingValue;
+            return amountToAdd / 2 * ((timeElapsed -= 2) * timeElapsed * (((s *= (1.525f)) + 1) * timeElapsed + s) + 2) + startingValue;
         }
     }
 }
