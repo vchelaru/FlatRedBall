f923996470e400c91975cc2f5ffba8796e10706d
Victor Chelaru <VicChelaru@gmail.com>
2023-05-27 12:36:14 -0600

    Added multi-page localization support.

--- Engines/FlatRedBallXNA/FlatRedBall/Localization/LocalizationManager.cs
+++ Engines/FlatRedBallXNA/FlatRedBall/Localization/LocalizationManager.cs
@@ -5,9 +5,27 @@ using System.Text;
 using FlatRedBall.IO.Csv;
 using System.Collections.ObjectModel;
 using FlatRedBall.Utilities;
+using Microsoft.VisualBasic;
 
 namespace FlatRedBall.Localization
 {
+    public class LocalizationEntry
+    {
+        public string Key { get; set; }
+
+        public List<string[]> Rows { get; set; } = new List<string[]>();
+
+        //public string[] Values { get; set; }
+        public override string ToString()
+        {
+            if (Rows.Count == 0) return "<no text>";
+            else
+            {
+                return Rows[0].FirstOrDefault();
+            }
+        }
+    }
+
     public static class LocalizationManager
     {
         #region Fields/Properties
@@ -18,7 +36,7 @@ namespace FlatRedBall.Localization
             set;
         }
 
-        static Dictionary<string, string[]> mStringDatabase = new Dictionary<string, string[]>();
+        static Dictionary<string, LocalizationEntry> mStringDatabase = new Dictionary<string, LocalizationEntry>();
 
         public static bool HasDatabase
         {
@@ -55,9 +73,10 @@ namespace FlatRedBall.Localization
 
             char oldDelimiter = CsvFileManager.Delimiter;
             CsvFileManager.Delimiter = delimiter;
-            Dictionary<string, string[]> entryDictionary = new Dictionary<string, string[]>();
 
-            CsvFileManager.CsvDeserializeDictionary<string, string[]>(fileName, entryDictionary, out rcr);
+            // We used to deserialize to a dictionary, but there could be rows that don't have a primary key for long text, so we're going to support that:
+            //CsvFileManager.CsvDeserializeDictionary<string, string[]>(fileName, entryDictionary, out rcr);
+            rcr = CsvFileManager.CsvDeserializeToRuntime(fileName);
             CsvFileManager.Delimiter = oldDelimiter;
 
 
@@ -68,13 +87,56 @@ namespace FlatRedBall.Localization
                 headerList.Add(header.Name);
             }
 
-            AddDatabase(entryDictionary, headerList);
+            Languages = new ReadOnlyCollection<string>(headerList);
+
+            mStringDatabase.Clear();
+
+            LocalizationEntry lastEntry = new LocalizationEntry();
+
+            foreach (var row in rcr.Records)
+            {
+                // assume the first row is the key:
+                var key = row[0];
+
+                var values = row.Skip(1).ToArray();
+
+                LocalizationEntry localizationEntry;
+                if(!string.IsNullOrEmpty(key))
+                {
+                    localizationEntry = new LocalizationEntry();
+                    localizationEntry.Key = key;
+                    mStringDatabase[key] = localizationEntry;
+                }
+                else
+                {
+                    localizationEntry = lastEntry;
+                }
+                localizationEntry.Rows.Add(values);
+                lastEntry = localizationEntry;
+            }
+
+
+            HasDatabase = true;
+        }
+
+        private static void CreateStringDatabase(Dictionary<string, string[]> entryDictionary)
+        {
+            mStringDatabase = new Dictionary<string, LocalizationEntry>();
+            foreach (var kvp in entryDictionary)
+            {
+                var entry = new LocalizationEntry
+                {
+                    Key = kvp.Key
+                };
+                entry.Rows.Add(kvp.Value);
+                mStringDatabase[kvp.Key] = entry;
+            }
         }
 
         public static void AddDatabase(Dictionary<string, string[]> entryDictionary, List<string> headerList)
         {
             Languages = new ReadOnlyCollection<string>(headerList);
-            mStringDatabase = entryDictionary;
+            CreateStringDatabase( entryDictionary);
             HasDatabase = true;
         }
 
@@ -85,33 +147,6 @@ namespace FlatRedBall.Localization
             mStringDatabase.Clear();
             HasDatabase = false;
             Languages = new ReadOnlyCollection<string>(new List<string>());
-        }
-
-        /// <summary>
-        /// Returns the translated string for the argument language. If stringId is null, "NULL STRING" is returned.
-        /// </summary>
-        /// <param name="stringID">The stringId to translate.</param>
-        /// <param name="language">The language index. Typically 0 is the string IDs, and languages begin with index 1</param>
-        /// <returns>The translated string.</returns>
-        public static string TranslateForLanguage(string stringID, int language)
-        {
-
-            if (stringID == null)
-            {
-                return "NULL STRING";
-            }
-            else if (mStringDatabase.ContainsKey(stringID))
-            {
-                return mStringDatabase[stringID][language];
-            }
-            else if (ShouldExcludeFromTranslation(stringID))
-            {
-                return stringID;
-            }
-            else
-            {
-                return stringID + " - UNTRANSLATED";
-            }
         }
 
         private static bool ShouldExcludeFromTranslation(string stringID)
@@ -162,7 +197,6 @@ namespace FlatRedBall.Localization
         public static string Translate(string stringID)
         {
             return TranslateForLanguage(stringID, CurrentLanguage);
-
         }
 
         public static string Translate(string stringID, params object[] args)
@@ -174,6 +208,56 @@ namespace FlatRedBall.Localization
             return translated;
         }
 
+        /// <summary>
+        /// Returns the translated string for the argument language. If stringId is null, "NULL STRING" is returned.
+        /// </summary>
+        /// <param name="stringID">The stringId to translate.</param>
+        /// <param name="language">The language index. Typically 0 is the string IDs, and languages begin with index 1</param>
+        /// <returns>The translated string.</returns>
+        public static string TranslateForLanguage(string stringID, int language)
+        {
+
+            if (stringID == null)
+            {
+                return "NULL STRING";
+            }
+            else if (mStringDatabase.ContainsKey(stringID))
+            {
+                return mStringDatabase[stringID].Rows[0][language];
+            }
+            else if (ShouldExcludeFromTranslation(stringID))
+            {
+                return stringID;
+            }
+            else
+            {
+                return stringID + " - UNTRANSLATED";
+            }
+        }
+
+        public static string[] TranslateMultiple(string stringID)
+        {
+            if (stringID == null)
+            {
+                return new string[] { "NULL STRING" };
+            }
+            else if (mStringDatabase.ContainsKey(stringID))
+            {
+                var entry = mStringDatabase[stringID];
+                var toReturn = entry.Rows.Select(item => item[0]);
+
+                return toReturn.ToArray();
+            }
+            else if (ShouldExcludeFromTranslation(stringID))
+            {
+                return new string[] { stringID };
+            }
+            else
+            {
+                return new string[] { stringID + " - UNTRANSLATED" };
+            }
+        }
+
 		/// <summary>
 		/// Grabs a dictionary with a copy of all the localization database keys and the current language values. 
 		/// </summary>
@@ -183,7 +267,7 @@ namespace FlatRedBall.Localization
 			var dict = new Dictionary<string, string>();
 			foreach (var key in mStringDatabase.Keys)
 			{
-				dict.Add(key, mStringDatabase[key][CurrentLanguage]);
+				dict.Add(key, mStringDatabase[key].Rows[0][CurrentLanguage]);
 			}
 			return dict;
 		}
