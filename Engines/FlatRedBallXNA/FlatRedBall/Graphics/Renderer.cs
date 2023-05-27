1ae16d587cfdadb0b8baa346b2a7be32ff52b750
Miguel Anxo Figueirido Prado <miguelfigueirido@hotmail.com>
2023-05-27 14:45:46 +0200

    Fixing the renderer assuming the shader will have the new format if the project is on MonoGame 3.8.1.

--- Engines/FlatRedBallXNA/FlatRedBall/Graphics/Renderer.cs
+++ Engines/FlatRedBallXNA/FlatRedBall/Graphics/Renderer.cs
@@ -693,6 +693,10 @@ namespace FlatRedBall.Graphics
 			}
         }
 
+        static EffectParameter mParameterCurrentTexture;
+
+        static bool mEffectHasNewformat;
+
         // Cached effect members to avoid list lookups while rendering
         static EffectTechnique mTechniqueTexture;
         static EffectTechnique mTechniqueAdd;
@@ -716,8 +720,6 @@ namespace FlatRedBall.Graphics
         static EffectTechnique mTechniqueColorTextureAlpha_Linear;
         static EffectTechnique mTechniqueInterpolateColor_Linear;
 
-        static EffectParameter mParameterCurrentTexture;
-
         static public Effect Effect
         {
             get { return mEffect; }
@@ -727,40 +729,52 @@ namespace FlatRedBall.Graphics
 
                 mParameterCurrentTexture = mEffect.Parameters["CurrentTexture"];
 
-#if MONOGAME_381
+                // Let's check if the shader has the new format (which includes
+                // separate versions of techniques for Point and Linear filtering).
+                // We try to cache the first technique in order to do so.
                 try { mTechniqueTexture = mEffect.Techniques["Texture_Point"]; } catch { }
-                try { mTechniqueAdd = mEffect.Techniques["Add_Point"]; } catch { }
-                try { mTechniqueSubtract = mEffect.Techniques["Subtract_Point"]; } catch { }
-                try { mTechniqueModulate = mEffect.Techniques["Modulate_Point"]; } catch { }
-                try { mTechniqueModulate2X = mEffect.Techniques["Modulate2X_Point"]; } catch { }
-                try { mTechniqueModulate4X = mEffect.Techniques["Modulate4X_Point"]; } catch { }
-                try { mTechniqueInverseTexture = mEffect.Techniques["InverseTexture_Point"]; } catch { }
-                try { mTechniqueColor = mEffect.Techniques["Color_Point"]; } catch { }
-                try { mTechniqueColorTextureAlpha = mEffect.Techniques["ColorTextureAlpha_Point"]; } catch { }
-                try { mTechniqueInterpolateColor = mEffect.Techniques["InterpolateColor_Point"]; } catch { }
 
-                try { mTechniqueTexture_Linear = mEffect.Techniques["Texture_Linear"]; } catch { }
-                try { mTechniqueAdd_Linear = mEffect.Techniques["Add_Linear"]; } catch { }
-                try { mTechniqueSubtract_Linear = mEffect.Techniques["Subtract_Linear"]; } catch { }
-                try { mTechniqueModulate_Linear = mEffect.Techniques["Modulate_Linear"]; } catch { }
-                try { mTechniqueModulate2X_Linear = mEffect.Techniques["Modulate2X_Linear"]; } catch { }
-                try { mTechniqueModulate4X_Linear = mEffect.Techniques["Modulate4X_Linear"]; } catch { }
-                try { mTechniqueInverseTexture_Linear = mEffect.Techniques["InverseTexture_Linear"]; } catch { }
-                try { mTechniqueColor_Linear = mEffect.Techniques["Color_Linear"]; } catch { }
-                try { mTechniqueColorTextureAlpha_Linear = mEffect.Techniques["ColorTextureAlpha_Linear"]; } catch { }
-                try { mTechniqueInterpolateColor_Linear = mEffect.Techniques["InterpolateColor_Linear"]; } catch { }
-#else
-                try { mTechniqueTexture = mEffect.Techniques["Texture"]; } catch { }
-                try { mTechniqueAdd = mEffect.Techniques["Add"]; } catch { }
-                try { mTechniqueSubtract = mEffect.Techniques["Subtract"]; } catch { }
-                try { mTechniqueModulate = mEffect.Techniques["Modulate"]; } catch { }
-                try { mTechniqueModulate2X = mEffect.Techniques["Modulate2X"]; } catch { }
-                try { mTechniqueModulate4X = mEffect.Techniques["Modulate4X"]; } catch { }
-                try { mTechniqueInverseTexture = mEffect.Techniques["InverseTexture"]; } catch { }
-                try { mTechniqueColor = mEffect.Techniques["Color"]; } catch { }
-                try { mTechniqueColorTextureAlpha = mEffect.Techniques["ColorTextureAlpha"]; } catch { }
-                try { mTechniqueInterpolateColor = mEffect.Techniques["InterpolateColor"]; } catch { }
-#endif
+                if (mTechniqueTexture != null)
+                {
+                    mEffectHasNewformat = true;
+
+                    //try { mTechniqueTexture = mEffect.Techniques["Texture_Point"]; } catch { } // This has been already cached
+                    try { mTechniqueAdd = mEffect.Techniques["Add_Point"]; } catch { }
+                    try { mTechniqueSubtract = mEffect.Techniques["Subtract_Point"]; } catch { }
+                    try { mTechniqueModulate = mEffect.Techniques["Modulate_Point"]; } catch { }
+                    try { mTechniqueModulate2X = mEffect.Techniques["Modulate2X_Point"]; } catch { }
+                    try { mTechniqueModulate4X = mEffect.Techniques["Modulate4X_Point"]; } catch { }
+                    try { mTechniqueInverseTexture = mEffect.Techniques["InverseTexture_Point"]; } catch { }
+                    try { mTechniqueColor = mEffect.Techniques["Color_Point"]; } catch { }
+                    try { mTechniqueColorTextureAlpha = mEffect.Techniques["ColorTextureAlpha_Point"]; } catch { }
+                    try { mTechniqueInterpolateColor = mEffect.Techniques["InterpolateColor_Point"]; } catch { }
+
+                    try { mTechniqueTexture_Linear = mEffect.Techniques["Texture_Linear"]; } catch { }
+                    try { mTechniqueAdd_Linear = mEffect.Techniques["Add_Linear"]; } catch { }
+                    try { mTechniqueSubtract_Linear = mEffect.Techniques["Subtract_Linear"]; } catch { }
+                    try { mTechniqueModulate_Linear = mEffect.Techniques["Modulate_Linear"]; } catch { }
+                    try { mTechniqueModulate2X_Linear = mEffect.Techniques["Modulate2X_Linear"]; } catch { }
+                    try { mTechniqueModulate4X_Linear = mEffect.Techniques["Modulate4X_Linear"]; } catch { }
+                    try { mTechniqueInverseTexture_Linear = mEffect.Techniques["InverseTexture_Linear"]; } catch { }
+                    try { mTechniqueColor_Linear = mEffect.Techniques["Color_Linear"]; } catch { }
+                    try { mTechniqueColorTextureAlpha_Linear = mEffect.Techniques["ColorTextureAlpha_Linear"]; } catch { }
+                    try { mTechniqueInterpolateColor_Linear = mEffect.Techniques["InterpolateColor_Linear"]; } catch { }
+                }
+                else
+                {
+                    mEffectHasNewformat = false;
+
+                    try { mTechniqueTexture = mEffect.Techniques["Texture"]; } catch { }
+                    try { mTechniqueAdd = mEffect.Techniques["Add"]; } catch { }
+                    try { mTechniqueSubtract = mEffect.Techniques["Subtract"]; } catch { }
+                    try { mTechniqueModulate = mEffect.Techniques["Modulate"]; } catch { }
+                    try { mTechniqueModulate2X = mEffect.Techniques["Modulate2X"]; } catch { }
+                    try { mTechniqueModulate4X = mEffect.Techniques["Modulate4X"]; } catch { }
+                    try { mTechniqueInverseTexture = mEffect.Techniques["InverseTexture"]; } catch { }
+                    try { mTechniqueColor = mEffect.Techniques["Color"]; } catch { }
+                    try { mTechniqueColorTextureAlpha = mEffect.Techniques["ColorTextureAlpha"]; } catch { }
+                    try { mTechniqueInterpolateColor = mEffect.Techniques["InterpolateColor"]; } catch { }
+                }
             }
         }
 
@@ -1303,9 +1317,10 @@ namespace FlatRedBall.Graphics
             EffectTechnique technique = null;
             bool useDefaultOrPointFilter = true;
 
-#if MONOGAME_381
-            useDefaultOrPointFilter = FlatRedBallServices.GraphicsOptions.TextureFilter == TextureFilter.Point;
-#endif
+            if (mEffectHasNewformat)
+            {
+                useDefaultOrPointFilter = FlatRedBallServices.GraphicsOptions.TextureFilter == TextureFilter.Point;
+            }
 
             switch (value)
             {
