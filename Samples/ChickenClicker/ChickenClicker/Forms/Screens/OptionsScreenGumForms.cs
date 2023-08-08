879cf15d7837659cfdd709375890ec79367e8440
Laurence <Laurence@WHELPSURFACE>
2023-08-08 19:36:47 +0100

    chicken clicker project

---
+++ Samples/ChickenClicker/ChickenClicker/Forms/Screens/OptionsScreenGumForms.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChickenClicker.FormsControls.Screens
+{
+    public partial class OptionsScreenGumForms
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}
---
+++ Samples/ChickenClicker/ChickenClicker/GumRuntimes/MenuScreenGumRuntime.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChickenClicker.GumRuntimes
+{
+    public partial class MenuScreenGumRuntime
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}
