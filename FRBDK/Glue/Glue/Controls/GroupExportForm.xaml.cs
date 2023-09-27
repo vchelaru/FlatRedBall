13e567faed04607005527398d062aad5f69e1776
Lucky Scamper <Lucasharskamp@gmail.com>
2023-09-27 19:51:46 +0200

    GroupExportForm converted from System.Window.Forms to WPF

---
+++ FRBDK/Glue/Glue/Controls/GroupExportForm.xaml.cs
@@ -0,0 +1,194 @@
+﻿using FlatRedBall.Glue.SaveClasses;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using FlatRedBall.Glue.Elements;
+using FlatRedBall.Glue.FormHelpers;
+using FlatRedBall.IO;
+using MouseEventArgs = System.Windows.Input.MouseEventArgs;
+using Gum.DataTypes;
+using ScreenSave = FlatRedBall.Glue.SaveClasses.ScreenSave;
+using Xceed.Wpf.Toolkit.Primitives;
+
+namespace FlatRedBall.Glue.Controls;
+/// <summary>
+/// Interaction logic for GroupExportForm.xaml
+///
+/// Invoked when the user selects the ''export group'' item under ''projects'' tab
+/// </summary>
+public partial class GroupExportForm
+{
+    #region Fields
+
+    private TreeViewItem _mScreensTreeNode;
+    private TreeViewItem _mEntitiesTreeNode;
+    private readonly char[] _splitChars = { '\\', '/' };
+
+    #endregion
+
+    public IEnumerable<GlueElement> SelectedElements => 
+        from TreeViewItem node 
+        in ToExportTreeView.Items 
+        select node.Tag as GlueElement;
+
+    public bool HasSelectedElements => ToExportTreeView.Items.Count != 0;
+
+    public GroupExportForm()
+    {
+        InitializeComponent();
+
+        FillAllAvailableTreeView();
+    }
+
+    private void FillAllAvailableTreeView()
+    {
+        var treeView = this.AllElementsTreeView;
+
+        _mScreensTreeNode = new TreeViewItem() {
+            Name = "Screens",
+            Header = CreateTreeViewItemContent("Screens")
+        };
+        treeView.Items.Add(_mScreensTreeNode);
+
+        foreach (ScreenSave screenSave in ObjectFinder.Self.GlueProject.Screens)
+        {
+            AddTreeNodeFor(screenSave);
+        }
+
+        _mEntitiesTreeNode = new TreeViewItem() {
+            Name = "Entities",
+            Header = CreateTreeViewItemContent("Entities"),
+
+        };
+        treeView.Items.Add(_mEntitiesTreeNode);
+
+        foreach (var entitySave in ObjectFinder.Self.GlueProject.Entities)
+        {
+            AddTreeNodeFor(entitySave);
+        }
+
+        PerformDeepSort(treeView.Items);
+    }
+
+    private static void PerformDeepSort(ItemCollection treeNodeCollection)
+    {
+        treeNodeCollection.SortByTextConsideringDirectories();
+
+        foreach (TreeViewItem treeNode in treeNodeCollection)
+        {
+            PerformDeepSort(treeNode.Items);
+        }
+    }
+
+    private void AddTreeNodeFor(EntitySave entitySave)
+    {
+        string fullName = entitySave.Name;
+
+        string directory = FileManager.GetDirectory(fullName, RelativeType.Relative);
+
+        var directoryNode = GetOrCreateDirectoryNode(directory, AllElementsTreeView.Items);
+        var newName = FileManager.RemovePath(entitySave.Name);
+        var elementNode = new TreeViewItem
+        {
+            Name = newName,
+            Header = CreateTreeViewItemContent(newName),
+            Tag = entitySave
+        };
+        directoryNode.Items.Add(elementNode);
+    }
+
+    private void AddTreeNodeFor(ScreenSave screenSave)
+    {
+        string fullName = screenSave.Name;
+
+        string directory = FileManager.GetDirectory(fullName, RelativeType.Relative);
+
+        TreeViewItem directoryNode = GetOrCreateDirectoryNode(directory, AllElementsTreeView.Items);
+
+        var newName = FileManager.RemovePath(screenSave.Name);
+        TreeViewItem elementNode = new TreeViewItem() {
+            Name = newName,
+            Header = CreateTreeViewItemContent(newName),
+            Tag = screenSave
+        };
+        directoryNode.Items.Add(elementNode);
+    }
+
+    private TreeViewItem GetOrCreateDirectoryNode(string directory, ItemCollection treeNodeCollection)
+    {
+        string[] splits = directory.Split(_splitChars, StringSplitOptions.RemoveEmptyEntries);
+        string currentCategory = splits[0];
+
+        var foundNode = treeNodeCollection
+            .Cast<TreeViewItem>()
+            .FirstOrDefault(subNode => String.Equals(subNode.Name, currentCategory, StringComparison.OrdinalIgnoreCase));
+
+        if (foundNode == null)
+        {
+            foundNode = new TreeViewItem();
+            foundNode.Name = currentCategory;
+            foundNode.Header = CreateTreeViewItemContent(currentCategory);
+            foundNode.Foreground = new SolidColorBrush(Color.FromRgb(255, 140, 0));
+            treeNodeCollection.Add(foundNode);
+        }
+
+        if (splits.Length == 1)
+        {
+            return foundNode;
+        }
+
+        int firstSlash = directory.IndexOfAny(_splitChars);
+        string subString = directory.Substring(firstSlash + 1);
+
+        return GetOrCreateDirectoryNode(subString, foundNode.Items);
+    }
+
+    private static object CreateTreeViewItemContent(string label)
+    {
+        return new Label() { Content = label };
+    }
+
+    private void AddAsSelectedElement(IElement element)
+    {
+        var node = new TreeViewItem
+        {
+            Tag = element,
+            Header = CreateTreeViewItemContent(element.Name)
+        };
+        ToExportTreeView.Items.Add(node);
+    }
+
+    private void OkButton_Click(object sender, EventArgs e)
+    {
+        this.DialogResult = true;
+        this.Close();
+    }
+
+    private void CancelButton_Click(object sender, EventArgs e)
+    {
+        this.DialogResult = false;
+        this.Close();
+    }
+
+    private TreeViewItem CurrentSelectedItem { get; set; }
+
+    private void AllElementsTreeView_OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+    {
+        CurrentSelectedItem = e.NewValue is TreeViewItem { Tag: not null } selectedItem ? selectedItem : null;
+    }
+
+    private void AllElementsTreeView_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        if (CurrentSelectedItem != null)
+        {
+            AddAsSelectedElement(CurrentSelectedItem.Tag as IElement);
+            (CurrentSelectedItem.Parent as TreeViewItem)!.Items.Remove(CurrentSelectedItem);
+            CurrentSelectedItem = null;
+        }
+    }
+}
--- FRBDK/Glue/Glue/Plugins/EmbeddedPlugins/TreeViewPlugin/TreeNodeCollectionExtensions.cs
+++ FRBDK/Glue/Glue/Plugins/EmbeddedPlugins/TreeViewPlugin/TreeNodeCollectionExtensions.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Windows.Controls;
 
 namespace FlatRedBall.Glue.FormHelpers
 {
     public static class TreeNodeCollectionExtensions
     {
+        [Obsolete("Lucky Scamper 09-23-23: remove thsi method once System.Web.Forms -> TreeView's are no longer used.")]
         public static void SortByTextConsideringDirectories(this TreeNodeCollection treeNodeCollection, bool recursive = false)
         {
             int lastObjectExclusive = treeNodeCollection.Count;
@@ -63,6 +65,60 @@ namespace FlatRedBall.Glue.FormHelpers
 
         }
 
+        public static void SortByTextConsideringDirectories(this ItemCollection treeNodeCollection, bool recursive = false)
+        {
+            int lastObjectExclusive = treeNodeCollection.Count;
+            for (int i = 0 + 1; i < lastObjectExclusive; i++)
+            {
+                TreeViewItem first = treeNodeCollection[i] as TreeViewItem;
+                TreeViewItem second = treeNodeCollection[i - 1] as TreeViewItem;
+                if (TreeNodeComparer(first, second) < 0)
+                {
+                    if (i == 1)
+                    {
+                        var treeNode = treeNodeCollection[i];
+                        treeNodeCollection.RemoveAt(i);
+
+                        treeNodeCollection.Insert(0, treeNode);
+                        continue;
+                    }
+
+                    for (int whereObjectBelongs = i - 2; whereObjectBelongs > -1; whereObjectBelongs--)
+                    {
+                        second = treeNodeCollection[whereObjectBelongs] as TreeViewItem;
+                        if (TreeNodeComparer(treeNodeCollection[i] as TreeViewItem, second) >= 0)
+                        {
+                            var treeNode = treeNodeCollection[i];
+
+                            treeNodeCollection.RemoveAt(i);
+                            treeNodeCollection.Insert(whereObjectBelongs + 1, treeNode);
+                            break;
+                        }
+
+                        if (whereObjectBelongs == 0 && TreeNodeComparer(treeNodeCollection[i] as TreeViewItem, treeNodeCollection[0] as TreeViewItem) < 0)
+                        {
+                            var treeNode = treeNodeCollection[i];
+                            treeNodeCollection.RemoveAt(i);
+                            treeNodeCollection.Insert(0, treeNode);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (recursive)
+            {
+                foreach (TreeViewItem node in treeNodeCollection)
+                {
+                    if (node.Items.Count > 0) // IsDirectoryNode();
+                    {
+                        SortByTextConsideringDirectories(node.Items, recursive);
+                    }
+                }
+            }
+
+        }
+
         [DllImport("Shlwapi.dll", CharSet = CharSet.Unicode)]
         private static extern int StrCmpLogicalW(string x, string y);
 
@@ -91,6 +147,22 @@ namespace FlatRedBall.Glue.FormHelpers
                 // This will put Level9 before Level10
                 return StrCmpLogicalW(first.Text, second.Text);
             }
+        }
+
+        private static int TreeNodeComparer(TreeViewItem first, TreeViewItem second)
+        {
+            bool isFirstDirectory = first.Items.Count > 0; // IsDirectoryNode();
+            bool isSecondDirectory = second.Items.Count > 0;
+
+            return isFirstDirectory switch
+            {
+                true when !isSecondDirectory => -1,
+                false when isSecondDirectory => 1,
+                _ => StrCmpLogicalW(first.Name, second.Name)
+            };
+
+            //return first.Text.CompareTo(second.Text);
+            // This will put Level9 before Level10
         }
 
         public static bool ContainsText(this TreeNodeCollection treeNodeCollection, string textToSearchFor)
