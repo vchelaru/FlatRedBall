58f73c46b4d890b717d54cf847e0a915a4a4479d
Victor Chelaru <VicChelaru@gmail.com>
2023-11-30 05:13:50 -0700

    Fixed crash on closing plugin creation window.

--- FRBDK/Glue/Glue/Plugins/EmbeddedPlugins/MenuStripPlugin/MainMenuStripPlugin.cs
+++ FRBDK/Glue/Glue/Plugins/EmbeddedPlugins/MenuStripPlugin/MainMenuStripPlugin.cs
@@ -104,7 +104,7 @@ namespace GlueFormsCore.Plugins.EmbeddedPlugins.MenuStripPlugin
             {
                 Plugins.Add(Localization.Texts.PluginInstall, () => new InstallPluginWindow().Show(MainGlueWindow.Self));
                 Plugins.Add(Localization.Texts.PluginUninstall, () => new UninstallPluginWindow().Show(MainGlueWindow.Self));
-                Plugins.Add(Localization.Texts.PluginCreate, () => new CreatePluginWindow().Show());
+                Plugins.Add(Localization.Texts.PluginCreate, () => new CreatePluginWindow().ShowDialog());
                 Plugins.DropDownItems.Add(new ToolStripSeparator());
             }
 
