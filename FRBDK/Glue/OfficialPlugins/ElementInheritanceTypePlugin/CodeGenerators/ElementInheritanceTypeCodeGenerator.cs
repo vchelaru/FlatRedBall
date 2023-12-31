1025b8ab93027249cbf77776b8ded870c2c87c0f
Victor Chelaru <VicChelaru@gmail.com>
2023-12-31 07:09:08 -0700

    Top down values are no longer (currently) generated in derived Type types.

--- FRBDK/Glue/OfficialPlugins/ElementInheritanceTypePlugin/CodeGenerators/ElementInheritanceTypeCodeGenerator.cs
+++ FRBDK/Glue/OfficialPlugins/ElementInheritanceTypePlugin/CodeGenerators/ElementInheritanceTypeCodeGenerator.cs
@@ -133,12 +133,21 @@ namespace OfficialPlugins.ElementInheritanceTypePlugin.CodeGenerators
 
         bool ShouldSkip(CustomVariable customVariable)
         {
-            switch(customVariable.Type)
+            var type = customVariable.Type;
+            switch (type)
             {
                 // Don't want to handle variables of certain types so we don't get unintentional loads:
                 case "FlatRedBall.TileGraphics.LayeredTileMap":
                     return true;
+
             }
+
+            if(type.EndsWith("DataTypes.TopDownValues"))
+            {
+                // This requires the content to have been loaded as well. We'll skip this for now, and come back to it later...
+                return true;
+            }
+
             return false;
         }
 
