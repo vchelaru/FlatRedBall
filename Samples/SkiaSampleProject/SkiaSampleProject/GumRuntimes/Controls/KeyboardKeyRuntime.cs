76f95ac401f47d2b4fae066ee1bf86131bd168d8
Victor Chelaru <VicChelaru@gmail.com>
2024-02-12 08:06:03 -0700

    SkiaSampleProject added

---
+++ Samples/SkiaSampleProject/SkiaSampleProject/GumRuntimes/Controls/KeyboardKeyRuntime.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SkiaSampleProject.GumRuntimes.Controls
+{
+    public partial class KeyboardKeyRuntime
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}
