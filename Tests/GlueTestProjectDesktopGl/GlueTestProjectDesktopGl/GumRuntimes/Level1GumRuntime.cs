0c902e2ff5fb455ab5803309ed0276a9235aa97e
Victor Chelaru <VicChelaru@gmail.com>
2024-02-25 08:09:13 -0700

    Added repro case for GameScreen state generating invalid Variant code.

---
+++ Tests/GlueTestProjectDesktopGl/GlueTestProjectDesktopGl/GumRuntimes/Level1GumRuntime.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GlueTestProject.GumRuntimes
+{
+    public partial class Level1GumRuntime
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}
---
+++ Tests/GlueTestProjectDesktopGl/GlueTestProjectDesktopGl/Screens/Level1.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using FlatRedBall;
+using FlatRedBall.Input;
+using FlatRedBall.Instructions;
+using FlatRedBall.AI.Pathfinding;
+using FlatRedBall.Graphics.Animation;
+using FlatRedBall.Gui;
+using FlatRedBall.Math;
+using FlatRedBall.Math.Geometry;
+using FlatRedBall.Localization;
+using Microsoft.Xna.Framework;
+
+using GlueTestProject.Entities;
+
+
+namespace GlueTestProject.Screens
+{
+    public partial class Level1
+    {
+
+        void CustomInitialize()
+        {
+
+
+        }
+
+        void CustomActivity(bool firstTimeCalled)
+        {
+
+
+        }
+
+        void CustomDestroy()
+        {
+
+
+        }
+
+        static void CustomLoadStaticContent(string contentManagerName)
+        {
+
+
+        }
+
+    }
+}
