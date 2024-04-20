ae2ecdd5efddb0ca57c7f2a141aa8088edf83080
Victor <VicChelaru@gmail.com>
2024-04-20 08:26:15 -0600

    First pass at new levels.

--- FRBDK/Glue/TileGraphicsPlugin/TileGraphicsPlugin/Content/Tilesets/FrbVisualTiles.tsx
+++ FRBDK/Glue/TileGraphicsPlugin/TileGraphicsPlugin/Content/Tilesets/FrbVisualTiles.tsx
@@ -1,4 +1,92 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="FrbVisualTiles" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
  <image source="FrbVisualTiles.png" width="512" height="512"/>
+ <tile id="418" probability="0.1"/>
+ <tile id="419" probability="0.1"/>
+ <tile id="488" probability="0.04"/>
+ <tile id="489" probability="0.007"/>
+ <tile id="495" probability="0.04"/>
+ <tile id="496" probability="0.007"/>
+ <wangsets>
+  <wangset name="Clouds" type="corner" tile="421">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="421" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="422" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="423" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="424" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="425" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="426" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="427" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="453" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="454" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="455" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="456" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="457" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="458" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="459" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="485" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="486" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="487" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="488" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="489" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="517" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="518" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="519" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="520" wangid="0,1,0,0,0,0,0,1"/>
+  </wangset>
+  <wangset name="HillsWithDirt" type="corner" tile="320">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="320" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="321" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="322" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="323" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="324" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="352" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="353" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="354" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="355" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="356" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="384" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="385" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="386" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="416" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="417" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="418" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="419" wangid="0,0,0,0,0,1,0,1"/>
+  </wangset>
+  <wangset name="HillsWithRock" type="corner" tile="448">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="448" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="449" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="450" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="451" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="452" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="480" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="481" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="482" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="483" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="484" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="512" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="513" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="514" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+  <wangset name="Bushes" type="corner" tile="428">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="428" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="429" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="430" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="431" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="432" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="460" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="461" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="462" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="463" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="464" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="492" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="493" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="494" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="495" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="496" wangid="0,1,0,1,0,1,0,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
