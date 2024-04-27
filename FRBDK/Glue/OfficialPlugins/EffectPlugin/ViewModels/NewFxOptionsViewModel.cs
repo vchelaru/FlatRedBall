5a0f232fd44b4fa2103debd949e9fd805f5892a3
treesgobark <loganpetro97@yahoo.com>
2024-04-27 17:30:49 -0400

    first attempt at additional effect options

--- FRBDK/Glue/OfficialPlugins/EffectPlugin/ViewModels/NewFxOptionsViewModel.cs
+++ FRBDK/Glue/OfficialPlugins/EffectPlugin/ViewModels/NewFxOptionsViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace OfficialPlugins.EffectPlugin.ViewModels
 {
@@ -30,7 +31,58 @@ namespace OfficialPlugins.EffectPlugin.ViewModels
                 return $"Include {fileName}.cs PostProcess file";
             }
         }
+        
+        public ShaderType ShaderType { get => Get<ShaderType>(); set => Set(value); }
+        
+        [DependsOn(nameof(ShaderType))]
+        public bool IsPostProcessingChecked
+        {
+            get => ShaderType == ShaderType.PostProcessing;
+            set
+            {
+                if (value)
+                {
+                    ShaderType = ShaderType.PostProcessing;
+                }
+            }
+        }
+        
+        [DependsOn(nameof(ShaderType))]
+        public bool IsSpriteChecked
+        {
+            get => ShaderType == ShaderType.Sprite;
+            set
+            {
+                if (value)
+                {
+                    ShaderType = ShaderType.Sprite;
+                }
+            }
+        }
+        
+        [DependsOn(nameof(ShaderType))]
+        public bool IsEmptyChecked
+        {
+            get => ShaderType == ShaderType.Empty;
+            set
+            {
+                if (value)
+                {
+                    ShaderType = ShaderType.Empty;
+                }
+            }
+        }
+
+        [DependsOn(nameof(IsPostProcessingChecked))]
+        public Visibility PostProcessOptionsVisibility => IsPostProcessingChecked.ToVisibility();
 
         // todo - realtime validation - does the file exist?
+    }
+
+    enum ShaderType
+    {
+        PostProcessing,
+        Sprite,
+        Empty,
     }
 }
