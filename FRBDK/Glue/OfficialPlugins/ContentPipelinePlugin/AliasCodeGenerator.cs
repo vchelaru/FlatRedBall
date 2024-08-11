1e37fe671e1f935b15795650b9b5e87c84f0aa65
Victor <VicChelaru@gmail.com>
2024-08-11 10:46:53 -0600

    Add file alias generation for all content pipeline.

--- FRBDK/Glue/OfficialPlugins/ContentPipelinePlugin/AliasCodeGenerator.cs
+++ FRBDK/Glue/OfficialPlugins/ContentPipelinePlugin/AliasCodeGenerator.cs
@@ -9,6 +9,8 @@ using FlatRedBall.IO;
 using FlatRedBall.Glue.Managers;
 using FlatRedBall.Glue.Plugins.ExportedInterfaces;
 using EditorObjects.IoC;
+using FlatRedBall.Glue.Plugins.ExportedImplementations;
+using FlatRedBall.Glue.Elements;
 
 namespace OfficialPlugins.ContentPipelinePlugin
 {
@@ -23,7 +25,9 @@ namespace OfficialPlugins.ContentPipelinePlugin
 
         public override void GenerateInitializeStart(ICodeBlock codeBlock)
         {
-            if(controller.Settings.UseContentPipelineOnAllPngs)
+            // See if any files use content pipeline
+            var anyUseContentPipeline = ObjectFinder.Self.GetAllReferencedFiles().Any(item => item.UseContentPipeline);
+            if(anyUseContentPipeline)
             {
                 var glueState = Container.Get<IGlueState>();
                 codeBlock.Line(glueState.ProjectNamespace + ".FileAliasLogic.SetFileAliases();");
@@ -39,7 +43,7 @@ namespace OfficialPlugins.ContentPipelinePlugin
             {
                 if(glueState.CurrentGlueProject != null)
                 {
-                    string codeFileContents = GetFileAliasLogicFileContents(forceUseContentPipelineOnPngs);
+                    string codeFileContents = GetFileAliasLogicFileContents();
 
                     glueCommands.ProjectCommands.CreateAndAddCodeFile("FileAliases.Generated.cs");
      
@@ -64,7 +68,7 @@ namespace OfficialPlugins.ContentPipelinePlugin
                 TaskExecutionPreference.AddOrMoveToEnd);
         }
 
-        private static string GetFileAliasLogicFileContents(bool isUsingContentPipeline)
+        private static string GetFileAliasLogicFileContents()
         {
             var glueState = Container.Get<IGlueState>();
 
@@ -74,22 +78,20 @@ namespace OfficialPlugins.ContentPipelinePlugin
 
             var codeBlock = classBlock.Function("public static void", "SetFileAliases", "");
 
-            var files = ContentPipelineController.GetReferencedPngs();
+            var fileNamesRelative = ObjectFinder.Self.GetAllReferencedFiles().Where(item => item.UseContentPipeline)
+                .Select(item => item.Name).ToHashSet();
 
             var contentFolder = glueState.ContentDirectory;
 
-            if(isUsingContentPipeline)
+            foreach (var fileName in fileNamesRelative)
             {
-                foreach (var file in files)
-                {
-                    var relativeFile = "content/" + FileManager.MakeRelative(file, contentFolder).ToLowerInvariant();
-                    string withExtension = relativeFile;
-                    string noExtension = FileManager.RemoveExtension(relativeFile);
-                    var line =
-                        $"FlatRedBall.Content.ContentManager.FileAliases.Add(FlatRedBall.IO.FileManager.Standardize(\"{withExtension}\"), " +
-                        $"FlatRedBall.IO.FileManager.Standardize(\"{noExtension}\"));";
-                    codeBlock.Line(line);
-                }
+                var relativeFile = "Content/" + fileName;
+                string withExtension = relativeFile;
+                string noExtension = FileManager.RemoveExtension(relativeFile);
+                var line =
+                    $"global::FlatRedBall.Content.ContentManager.FileAliases.Add(global::FlatRedBall.IO.FileManager.Standardize(\"{withExtension}\"), " +
+                    $"global::FlatRedBall.IO.FileManager.Standardize(\"{noExtension}\"));";
+                codeBlock.Line(line);
             }
 
             var codeFileContents = namespaceBlock.ToString();
