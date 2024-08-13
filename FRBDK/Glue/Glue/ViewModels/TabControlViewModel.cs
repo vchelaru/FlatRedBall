807011caecd6211c7b241950046f4a7d29a9f644
victor <vicdotexe@gmail.com>
2024-08-13 01:30:01 -0700

    Refactor tab-related mvvm and xaml bindings.

--- FRBDK/Glue/Glue/ViewModels/TabControlViewModel.cs
+++ FRBDK/Glue/Glue/ViewModels/TabControlViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using FlatRedBall.Glue.Controls;
 using FlatRedBall.Glue.FormHelpers;
 using FlatRedBall.Glue.MVVM;
+using FlatRedBall.Glue.Plugins;
 using FlatRedBall.Glue.Plugins.ExportedImplementations;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -12,7 +14,7 @@ using System.Windows;
 namespace GlueFormsCore.ViewModels
 {
 
-    public class TabContainerViewModel
+    public class TabContainerViewModel : ViewModel
     {
         public PluginTabPage this[int index]
         {
@@ -21,10 +23,29 @@ namespace GlueFormsCore.ViewModels
         }
         public void Add(PluginTabPage item) => Tabs.Add(item);
         public void Remove(PluginTabPage item) => Tabs.Remove(item);
+
+        public PluginTabPage? SelectedTab
+        {
+            get => Get<PluginTabPage>();
+            set => Set(value);
+        }
+
+        public TabLocation Location { get; init; }
+
         public int Count => Tabs.Count;
         public ObservableCollection<PluginTabPage> Tabs { get; private set; } = new ObservableCollection<PluginTabPage>();
 
         public Dictionary<string, PluginTabPage> TabsForTypes { get; private set; } = new Dictionary<string, PluginTabPage>();
+
+        public TabContainerViewModel()
+        {
+            Tabs.CollectionChanged += (_, _) =>
+            {
+                NotifyPropertyChanged(nameof(Count));
+                SelectedTab ??= Tabs.FirstOrDefault();
+            };
+        }
+
         public void SetTabForCurrentType(PluginTabPage tab)
         {
             var treeNode = GlueState.Self.CurrentTreeNode;
@@ -35,6 +56,29 @@ namespace GlueFormsCore.ViewModels
                 TabsForTypes[selectedType] = tab;
             }
         }
+
+        public void ShowMostRecentTabFor(string typeName)
+        {
+            if (Count < 2)
+            {
+                return;
+            }
+
+            if (!TabsForTypes.TryGetValue(typeName, out PluginTabPage tab)) 
+            { 
+                List<PluginTabPage> ordered = Tabs
+                    .OrderBy(item => !item.IsPreferredDisplayerForType(typeName))
+                    .ThenByDescending(item => item.LastTimeClicked)
+                    .ToList();
+
+                if (ordered[0].LastTimeClicked != ordered[1].LastTimeClicked)
+                {
+                    tab = ordered[0];
+                }
+            }
+
+            SelectedTab = tab;
+        }
     }
 
     public class TabControlViewModel : ViewModel
@@ -42,47 +86,11 @@ namespace GlueFormsCore.ViewModels
         public static bool IsRecordingSelection { get; set; } = true;
         #region Fields/Properties
 
-        public TabContainerViewModel TopTabItems { get; private set; } =    new TabContainerViewModel();
-        public TabContainerViewModel BottomTabItems { get; private set; } = new TabContainerViewModel();
-        public TabContainerViewModel LeftTabItems { get; private set; } =   new TabContainerViewModel();
-        public TabContainerViewModel RightTabItems { get; private set; } =  new TabContainerViewModel();
-        public TabContainerViewModel CenterTabItems { get; private set; } = new TabContainerViewModel();
-
-        public PluginTabPage TopSelectedTab
-        {
-            get => Get<PluginTabPage>();
-            set => Set(value);
-        }
-
-        public PluginTabPage BottomSelectedTab
-        {
-            get => Get<PluginTabPage>();
-            set => Set(value);
-        }
-
-        public PluginTabPage LeftSelectedTab
-        {
-            get => Get<PluginTabPage>();
-            set => Set(value);
-        }
-
-        public PluginTabPage RightSelectedTab
-        {
-            get => Get<PluginTabPage>();
-            set => Set(value);
-        }
-
-        public PluginTabPage CenterSelectedTab
-        {
-            get => Get<PluginTabPage>();
-            set => Set(value);
-        }
-
-        public GridLength TopSplitterHeight
-        {
-            get => Get<GridLength>();
-            set => Set(value);
-        }
+        public TabContainerViewModel TopTabItems { get; } = new() { Location = TabLocation.Top };
+        public TabContainerViewModel BottomTabItems { get; } = new () { Location = TabLocation.Bottom };
+        public TabContainerViewModel LeftTabItems { get; } = new () { Location = TabLocation.Left };
+        public TabContainerViewModel RightTabItems { get; } =  new () { Location = TabLocation.Right };
+        public TabContainerViewModel CenterTabItems { get; } = new () { Location = TabLocation.Center };
 
         public GridLength TopPanelHeight
         {
@@ -96,13 +104,7 @@ namespace GlueFormsCore.ViewModels
             set => Set(value);
         }
 
-        public GridLength LeftSplitterWidth
-        {
-            get => Get<GridLength>();
-            set => Set(value);
-        }
-
-        public GridLength BottomSplitterHeight
+        public GridLength RightPanelWidth
         {
             get => Get<GridLength>();
             set => Set(value);
@@ -118,166 +120,55 @@ namespace GlueFormsCore.ViewModels
 
         public TabControlViewModel()
         {
-            TopTabItems.Tabs.CollectionChanged += (_, __) => NotifyPropertyChanged(nameof(TopTabItems));
-            BottomTabItems.Tabs.CollectionChanged += (_, __) => NotifyPropertyChanged(nameof(BottomTabItems));
-            LeftTabItems.Tabs.CollectionChanged += (_, __) => NotifyPropertyChanged(nameof(LeftTabItems));
-            RightTabItems.Tabs.CollectionChanged += (_, __) => NotifyPropertyChanged(nameof(RightTabItems));
-            CenterTabItems.Tabs.CollectionChanged += (_, __) => NotifyPropertyChanged(nameof(CenterTabItems));
-
-            this.PropertyChanged += (sender, args) => HandlePropertyChanged(args.PropertyName);
-
-            ExpandAndCollapseColumnAndRowWidths();
+            TopTabItems.Tabs.CollectionChanged += (_, args) => AdjustGrid(TopTabItems, args);
+            LeftTabItems.Tabs.CollectionChanged += (_, args) => AdjustGrid(LeftTabItems, args);
+            RightTabItems.Tabs.CollectionChanged += (_, args) => AdjustGrid(RightTabItems, args);
+            CenterTabItems.Tabs.CollectionChanged += (_, args) => AdjustGrid(CenterTabItems, args);
+            BottomTabItems.Tabs.CollectionChanged += (_, args) => AdjustGrid(BottomTabItems, args);
         }
 
-        private void HandlePropertyChanged(string propertyName)
+        private void AdjustGrid(TabContainerViewModel tab, NotifyCollectionChangedEventArgs args)
         {
-            switch (propertyName)
+            GridLength? gridLength = null;
+
+            if (args.NewItems is not null && tab.Count == 1)
             {
-                case nameof(TopTabItems):
-                    ExpandAndCollapseColumnAndRowWidths();
-                    if (TopTabItems.Count > 0 && TopSelectedTab == null)
-                    {
-                        TopSelectedTab = TopTabItems[0];
-                    }
+                int length = tab.Location == TabLocation.Left ? 230 : 200;
+                gridLength = new GridLength(length, GridUnitType.Pixel);
+            }
+            else if (tab.Count == 0)
+            {
+                gridLength = new GridLength(0, GridUnitType.Pixel);
+            }
+
+            if (gridLength is not { } gl) return;
+
+            switch (tab.Location)
+            {
+                case TabLocation.Left:
+                    LeftPanelWidth = gl;
                     break;
-                case nameof(BottomTabItems):
-                    ExpandAndCollapseColumnAndRowWidths();
-                    if (BottomTabItems.Count > 0 && BottomSelectedTab == null)
-                    {
-                        BottomSelectedTab = BottomTabItems[0];
-                    }
+                case TabLocation.Right:
+                    RightPanelWidth = gl;
                     break;
-                case nameof(LeftTabItems):
-                    ExpandAndCollapseColumnAndRowWidths();
-                    if (LeftTabItems.Count > 0 && LeftSelectedTab == null)
-                    {
-                        LeftSelectedTab = LeftTabItems[0];
-                    }
+                case TabLocation.Top:
+                    TopPanelHeight = gl;
                     break;
-                case nameof(RightTabItems):
-                    ExpandAndCollapseColumnAndRowWidths();
-                    if (RightTabItems.Count > 0 && RightSelectedTab == null)
-                    {
-                        RightSelectedTab = RightTabItems[0];
-                    }
-                    break;
-                case nameof(CenterTabItems):
-                    ExpandAndCollapseColumnAndRowWidths();
-                    if (CenterTabItems.Count > 0 && CenterSelectedTab == null)
-                    {
-                        CenterSelectedTab = CenterTabItems[0];
-                    }
+                case TabLocation.Bottom:
+                    BottomPanelHeight = gl;
                     break;
             }
-        }
-
-        double? leftPixelWhenShrank;
-
-        private void ExpandAndCollapseColumnAndRowWidths()
-        {
-            var shouldShrinkLeft = LeftTabItems.Count == 0 && LeftSplitterWidth.Value > 0;
-            var shouldExpandLeft = LeftTabItems.Count > 0 && LeftSplitterWidth.Value == 0;
-
-            var shouldShrinkTop = TopTabItems.Count == 0 && TopSplitterHeight.Value > 0;
-            var shouldExpandTop = TopTabItems.Count > 0 && TopSplitterHeight.Value == 0;
-
-            var shouldShrinkBottom = BottomTabItems.Count == 0 && BottomSplitterHeight.Value > 0;
-            var shouldExpandBottom = BottomTabItems.Count > 0 && BottomSplitterHeight.Value == 0;
-
-            if (shouldShrinkLeft)
-            {
-                if(LeftPanelWidth.Value > 1)
-                {
-                    leftPixelWhenShrank = LeftPanelWidth.Value;
-                }
-                LeftSplitterWidth = new GridLength(0);
-                LeftPanelWidth = new GridLength(0, GridUnitType.Pixel);
-
-            }
-            else if (shouldExpandLeft)
-            {
-                LeftSplitterWidth = new GridLength(4);
-                LeftPanelWidth = new GridLength(leftPixelWhenShrank ?? 230, GridUnitType.Pixel);
-                //LeftPanelWidth = new GridLength(1, GridUnitType.Star);
-            }
-
-            if (shouldShrinkTop)
-            {
-                TopSplitterHeight = new GridLength(0);
-                TopPanelHeight = new GridLength(0);
-            }
-            else if (shouldExpandTop)
-            {
-                TopSplitterHeight = new GridLength(4);
-                //TopPanelHeight = new GridLength(1, GridUnitType.Star);
-                TopPanelHeight = new GridLength(200, GridUnitType.Pixel);
-            }
-
-            if (shouldShrinkBottom)
-            {
-                BottomSplitterHeight = new GridLength(0);
-                BottomPanelHeight = new GridLength(0);
-            }
-            else if (shouldExpandBottom)
-            {
-                BottomSplitterHeight = new GridLength(4);
-                //BottomPanelHeight = new GridLength(1, GridUnitType.Star);
-                BottomPanelHeight = new GridLength(200, GridUnitType.Pixel);
-            }
-
         }
 
         internal void UpdateToSelection(ITreeNode selectedTreeNode)
         {
-            var selectedType = selectedTreeNode?.Tag?.GetType().Name ?? selectedTreeNode?.Text;
-
-            ShowMostRecentTabFor(TopTabItems,
-                (item) => TopSelectedTab = item, 
-                selectedType);
-
-            ShowMostRecentTabFor(BottomTabItems,
-                (item) => BottomSelectedTab = item, 
-                selectedType);
-
-            ShowMostRecentTabFor(LeftTabItems,
-                (item) => LeftSelectedTab = item, 
-                selectedType);
-
-            ShowMostRecentTabFor(CenterTabItems,
-                (item) => CenterSelectedTab = item, 
-                selectedType);
-
-            ShowMostRecentTabFor(RightTabItems,
-                (item) => RightSelectedTab = item, 
-                selectedType);
-        }
-
-
-        private static void ShowMostRecentTabFor(TabContainerViewModel items, Action<PluginTabPage> action, string typeName)
-        {
-            if (items.Count > 1)
-            {
-                // Is there a tab for this type?
-                if(typeName != null && items.TabsForTypes.ContainsKey(typeName))
-                {
-                    action(items.TabsForTypes[typeName]);
-
-                }
-                else
-                {
-
-                    var ordered = items.Tabs
-                        .OrderBy(item => !item.IsPreferredDisplayerForType(typeName))
-                        .ThenByDescending(item => item.LastTimeClicked).ToList();
-
-                    if (ordered[0].LastTimeClicked != ordered[1].LastTimeClicked)
-                    {
-                        action(ordered[0]);
-                    }
-                }
-            }
-
+            string selectedType = selectedTreeNode?.Tag?.GetType().Name ?? selectedTreeNode?.Text;
+            
+            TopTabItems.ShowMostRecentTabFor(selectedType);
+            BottomTabItems.ShowMostRecentTabFor(selectedType);
+            LeftTabItems.ShowMostRecentTabFor(selectedType);
+            CenterTabItems.ShowMostRecentTabFor(selectedType);
+            RightTabItems.ShowMostRecentTabFor(selectedType);
         }
     }
-
 }
