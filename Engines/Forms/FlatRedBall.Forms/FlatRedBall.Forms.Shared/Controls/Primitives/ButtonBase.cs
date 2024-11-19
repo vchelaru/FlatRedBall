f718a7cae97f056598c3969938611c0aa867ce3b
Victor <VicChelaru@gmail.com>
2024-11-19 04:52:21 -0700

    ButtonBase is unified!

--- Engines/Forms/FlatRedBall.Forms/FlatRedBall.Forms.Shared/Controls/Primitives/ButtonBase.cs
+++ Engines/Forms/FlatRedBall.Forms/FlatRedBall.Forms.Shared/Controls/Primitives/ButtonBase.cs
@@ -241,6 +241,13 @@ public class ButtonBase : FrameworkElement, IInputReceiver
         IsFocused = false;
     }
 
+#if !FRB
+    public void DoKeyboardAction(IInputReceiverKeyboard keyboard)
+    {
+
+    }
+#endif
+
     public void ReceiveInput()
     {
     }
@@ -258,5 +265,5 @@ public class ButtonBase : FrameworkElement, IInputReceiver
     {
     }
 
-    #endregion
+#endregion
 }
