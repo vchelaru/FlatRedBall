70b25a9ad2f61b8da066298519b52306b2299fac
Justin Johnson <justin.d.johnson@gmail.com>
2025-05-24 12:40:56 -0600

    Fixed some issues preventing compiling for netstandard2.0

---
+++ Engines/Forms/FlatRedBall.Forms/FlatRedBall.Forms/CompilerServices.cs
@@ -0,0 +1,7 @@
+﻿// 2025-05-24 Justin: this is required to compile Forms for netstandard because
+// this property only existed after .NET Core 5. See:
+// https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit { }
+}
