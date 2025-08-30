1c13ba9fa07d28614cd6e3fe1d47faf13f70f56d
Victor Chelaru <VicChelaru@gmail.com>
2025-08-30 10:45:27 -0600

    Unit test updated!

--- Tests/EngineUnitTests/Math/Geometry/PolygonTests.cs
+++ Tests/EngineUnitTests/Math/Geometry/PolygonTests.cs
@@ -15,7 +15,7 @@ public class PolygonTests
         var rectangle = new AxisAlignedRectangle();
 
         rectangle.X = 416;
-        rectangle.Width = 7;
+        rectangle.Width = 14;
 
         rectangle.Y = -545;
         rectangle.Height = 32;
@@ -37,4 +37,35 @@ public class PolygonTests
 
         rectangle.Y.ShouldBe(-544);
     }
+
+
+    [Fact]
+    public void CollideAgainstMove_ShouldRepositionRectangleCorrectly2()
+    {
+        var rectangle = new AxisAlignedRectangle();
+
+        rectangle.X = 409;
+        rectangle.Y = -544;
+        rectangle.Width = 14;
+
+        rectangle.Height = 32;
+
+        var polygon = new Polygon();
+        polygon.Y = -568;
+        polygon.X = 424;
+
+        polygon.Points = new List<Point>
+        {
+            new Point(-8, 8),
+            new Point(8, 0),
+            new Point(8, -8),
+            new Point(-8, -8),
+            //new Point(-8, 8),
+        };
+
+        rectangle.CollideAgainstMove(polygon, 0, 1);
+
+        rectangle.Y.ShouldBe(-544);
+    }
+
 }
