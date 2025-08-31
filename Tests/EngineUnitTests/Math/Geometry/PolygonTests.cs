5bd2a1fe1ca2b1edc8fc1c235c67086d3186259b
Bryan Yeo <48835025+bransay@users.noreply.github.com>
2025-08-31 15:05:21 -0700

    bryan's SAT implementation (#1800)
    * bryan's SAT implementation
    
    * hacked to always apply SAT -- not sure if the concave detection was working right?
    
    * Improved top of polygon collision
    
    * Clean tests (#1801)
    
    * Removed lots of unused tests
    
    Added new unit test project
    
    * Fixed Glue auto test
    
    * Added unit test
    
    * Unit test updated!
    
    * Updated unit test
    
    * mtv 0 means no collision -- dont treat it like some special case
    
    * Standing on slope now resets velocity to the surface of the platform.
    
    * Platformer collision against polygon entities now consider standing on slopes
    
    ---------
    
    Co-authored-by: Bryan Yeo <bryandismas@simiansoftwerks.com>
    Co-authored-by: Victor Chelaru <VicChelaru@gmail.com>

---
+++ Tests/EngineUnitTests/Math/Geometry/PolygonTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlatRedBall.Math.Geometry;
+using Shouldly;
+
+namespace EngineUnitTests.Math.Geometry;
+public class PolygonTests
+{
+    [Fact]
+    public void CollideAgainstMove_ShouldRepositionRectangleCorrectly()
+    {
+        var rectangle = new AxisAlignedRectangle();
+
+        rectangle.X = 416;
+        rectangle.Width = 14;
+
+        rectangle.Y = -545;
+        rectangle.Height = 32;
+
+        var polygon = new Polygon();
+        polygon.Y = -568;
+        polygon.X = 424;
+
+        polygon.Points = new List<Point>
+        {
+            new Point(-8, 8),
+            new Point(8, 0),
+            new Point(8, -8),
+            new Point(-8, -8),
+            new Point(-8, 8),
+        };
+
+        rectangle.CollideAgainstMove(polygon, 0, 1);
+
+        rectangle.Y.ShouldBe(-544);
+    }
+
+
+    [Fact]
+    public void CollideAgainstMove_ShouldRepositionRectangleCorrectly2()
+    {
+        var rectangle = new AxisAlignedRectangle();
+
+        rectangle.X = 409;
+        rectangle.Y = -544;
+        rectangle.Width = 14;
+
+        rectangle.Height = 32;
+
+        var polygon = new Polygon();
+        polygon.Y = -568;
+        polygon.X = 424;
+
+        polygon.Points = new List<Point>
+        {
+            new Point(-8, 8),
+            new Point(8, 0),
+            new Point(8, -8),
+            new Point(-8, -8),
+            new Point(-8, 8),
+        };
+
+        rectangle.CollideAgainstMove(polygon, 0, 1);
+
+        rectangle.Y.ShouldBe(-544);
+    }
+
+}
