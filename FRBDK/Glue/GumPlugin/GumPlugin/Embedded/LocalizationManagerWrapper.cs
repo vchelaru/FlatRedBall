a52e8a5184edb58e7458dc324848a83686f1f59b
Victor Chelaru <VicChelaru@gmail.com>
2026-02-09 08:53:36 -0700

    Updated to latest Gum localization code

--- FRBDK/Glue/GumPlugin/GumPlugin/Embedded/LocalizationManagerWrapper.cs
+++ FRBDK/Glue/GumPlugin/GumPlugin/Embedded/LocalizationManagerWrapper.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using FlatRedBall.Localization;
-using MonoGameGum.Localization;
+using Gum.Localization;
 
 namespace GumCoreShared.FlatRedBall.Embedded;
 
