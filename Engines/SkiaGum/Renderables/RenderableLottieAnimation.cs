5bfba84a3a4693653921fbdd88c081235db74cc6
Victor Chelaru <VicChelaru@gmail.com>
2026-02-19 22:19:45 -0700

    fixed Skiagum issues

--- Engines/SkiaGum/Renderables/RenderableLottieAnimation.cs
+++ Engines/SkiaGum/Renderables/RenderableLottieAnimation.cs
@@ -90,7 +90,7 @@ namespace SkiaGum.Renderables
                     // todo - support MonoGame loads here using the normal content loader...
 #if GUM
                     var sourceFileAbsolute =
-                        FileManager.RemoveDotDotSlash(Gum.Services.Locator.GetRequiredService<Gum.ToolStates.IProjectState>().ProjectDirectory + sourceFile);
+                        FileManager.RemoveDotDotSlash(Gum.ToolStates.ProjectState.Self.ProjectDirectory + sourceFile);
                     if(System.IO.File.Exists(sourceFileAbsolute))
                     {
                         animation = SkiaSharp.Skottie.Animation.Create(sourceFileAbsolute);
